7678122089ff97e9019b6c12986c37010b6c1dcd
shalabhs <shalabhs@microsoft.com>
2019-08-12 23:29:29 -0700

    Adding the Outgoing Message Header interface and concrete header class

---
+++ src/Microsoft.Actions.Actors/Communication/IMessageHeader.cs
@@ -0,0 +1,19 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Actions.Actors.Communication
+{
+    /// <summary>
+    /// Defines an interface that must be implemented to provide message header for the serialized Message.
+    /// </summary>
+    internal interface IMessageHeader
+    {
+        /// <summary>
+        /// Returns the byte array to be sent over the wire.
+        /// </summary>
+        /// <returns>Byte Array.</returns>
+        byte[] GetSendBytes();
+    }
+}
---
+++ src/Microsoft.Actions.Actors/Communication/OutgoingMessageHeader.cs
@@ -0,0 +1,27 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Actions.Actors.Communication
+{
+    internal sealed class OutgoingMessageHeader : IMessageHeader
+    {
+        private readonly byte[] outgoingBuffer;
+
+        public OutgoingMessageHeader()
+        {
+            this.outgoingBuffer = null;
+        }
+
+        public OutgoingMessageHeader(byte[] buffer)
+        {
+            this.outgoingBuffer = buffer;
+        }
+
+        public byte[] GetSendBytes()
+        {
+            return this.outgoingBuffer;
+        }
+    }
+}
