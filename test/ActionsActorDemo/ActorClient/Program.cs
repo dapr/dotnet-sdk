0f63618fa5b660452f4bb0b583f3867b8314b101
Aman Bhardwaj <amanbha@users.noreply.github.com>
2019-08-30 17:29:02 -0700

    Updating the client code

--- test/ActionsActorDemo/ActorClient/Program.cs
+++ test/ActionsActorDemo/ActorClient/Program.cs
@@ -16,33 +16,18 @@ namespace ActorClient
             {
                 PropertyA = "ValueA",
                 PropertyB = "ValueBCDEF",
-            };
-
-
-            // Invoke without Remoting, cross language invocation.
-             var proxy = ActorProxy.Create(new ActorId("abc"), "DemoActor");
-            var resData = proxy.InvokeAsync<MyData>("Echo", data).GetAwaiter().GetResult();
-            proxy.InvokeAsync("RegisterTimer").GetAwaiter().GetResult();                                  
-            proxy.InvokeAsync("UnregisterReminder").GetAwaiter().GetResult();
-            proxy.InvokeAsync("NoReturnTypeNoArg").GetAwaiter().GetResult();
-            proxy.InvokeAsync<string>("DoesntExist", data).GetAwaiter().GetResult();
-            try
-            {
-                proxy.InvokeAsync<string>("ThrowException", data).GetAwaiter().GetResult();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            
-             var result = proxy.InvokeAsync<string>("ProcessData", data).GetAwaiter().GetResult();
-             //var resData = proxy.InvokeAsync<MyData>("GetData", null).GetAwaiter().GetResult();
-
+            };           
 
             // Remoting Calls
             var proxyRemoting = ActorProxy.Create<IDemoActor>(new ActorId("abc"), "DemoActor");
             var echoResult = proxyRemoting.Echo(data).GetAwaiter().GetResult();
             //proxy.ProcessData(data).GetAwaiter().GetResult();
+            
+            
+            
+            // Invoke without Remoting, these invocations need method name.  NOT NEEDED FOR DEMO FOR REMOTING
+            var proxy = ActorProxy.Create(new ActorId("abc"), "DemoActor");
+            var resData = proxy.InvokeAsync<MyData>("Echo", data).GetAwaiter().GetResult();
         }
     }
 }
