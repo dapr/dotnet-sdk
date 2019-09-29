d00d8b3a8072baaa4bbcee9317e0d70aa6a4d862
Ryan Nowak <nowakra@gmail.com>
2019-09-28 20:07:20 -0700

    Add project skeletons
    Decoder-ring for projects:
    
    Microsoft.Dapr.Client[.Test] - client and tests
    Microsoft.Dapr.AspNetCore[.Test] ASP.NET Core integration and tests
    Microsoft.Dapr.AspNetCore.IntegrationTest[.App] integration tests
    
    RoutingSample - sample using route-to-code
    ControllerSample - sample using controllers

---
+++ samples/RoutingSample/Program.cs
@@ -0,0 +1,14 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace RoutingSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
