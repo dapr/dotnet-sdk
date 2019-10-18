e3446c5bd643e8ebb2f882c73f2dfe5b80002690
Aman Bhardwaj <amanbha@users.noreply.github.com>
2019-10-17 17:11:37 -0700

    Update Program.cs

--- samples/Actor/ActorClient/Program.cs
+++ samples/Actor/ActorClient/Program.cs
@@ -36,9 +36,9 @@ namespace ActorClient
             // Make strongly typed Actor calls with Remoting.
             // DemoACtor is the type registered with Dapr runtime in the service.
             var proxy = ActorProxy.Create<IDemoActor>(actorId, "DemoActor");
-            Console.WriteLine("Making call using actor proxy to save date.");
+            Console.WriteLine("Making call using actor proxy to save data.");
             proxy.SaveData(data).GetAwaiter().GetResult();
-            Console.WriteLine("Making call using actor proxy to get date.");
+            Console.WriteLine("Making call using actor proxy to get data.");
             var receivedData = proxy.GetData().GetAwaiter().GetResult();
             Console.WriteLine($"Received data is {receivedData.ToString()}");
         }
