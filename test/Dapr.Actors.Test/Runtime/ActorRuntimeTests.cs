0173a0d4ffedf93d39b2568386d5a693487ed6c7
Phillip Hoff <phillip@orst.edu>
2019-11-19 16:12:08 -0800

    Add test for inferred actor type.

---
+++ test/Dapr.Actors.Test/Runtime/ActorRuntimeTests.cs
@@ -0,0 +1,41 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace Dapr.Actors.Test
+{
+    using System;
+    using Dapr.Actors;
+    using Dapr.Actors.Runtime;
+    using Xunit;
+
+    public sealed class ActorRuntimeTests
+    {
+        private interface ITestActor : IActor
+        {
+        }
+
+        [Fact]
+        public void TestInferredActorType()
+        {
+            var actorRuntime = new ActorRuntime();
+
+            string actorTypeName = typeof(TestActor).Name;
+
+            Assert.DoesNotContain(actorTypeName, actorRuntime.RegisteredActorTypes, StringComparer.InvariantCulture);
+
+            actorRuntime.RegisterActor<TestActor>();
+
+            Assert.Contains(actorTypeName, actorRuntime.RegisteredActorTypes, StringComparer.InvariantCulture);
+        }
+
+        private sealed class TestActor : Actor, ITestActor
+        {
+            public TestActor(ActorService actorService, ActorId actorId)
+                : base(actorService, actorId)
+            {
+            }
+        }
+    }
+}
