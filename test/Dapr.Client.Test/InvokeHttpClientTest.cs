5562d0d64b3320298cc1a5b9fc20cf7fe59e5681
James Eastham <j.eastham1993@gmail.com>
2019-12-13 18:05:17 +0000

    Issue 184 (#185)
    * Add InvokeClient and InvokeHttpClient dapr #184
    
    * Add tests for InvokeHttpClient dapr#184
    
    * Update namespace of InvokeHttpClient  dapr#184
    
    * Add DI of InvokeHttpClient dapr#184
    
    * Resolve code format issues dapr#184
    
    * Remove InvokeEnvelope and add params to InvokeMethod dapr#184
    
    * Update method signature to use generic types for Request/Response dapr#184
    
    * Update parameter in data null check dapr#184

---
+++ test/Dapr.Client.Test/InvokeHttpClientTest.cs
@@ -0,0 +1,151 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace Dapr.Client.Test
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Xunit;
+
+    public class InvokeHttpClientTest
+    {
+        [Fact]
+        public async Task InvokeMethodAsync_CanInvokeMethodWithReturnTypeAndData()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokedResponse>("test", "test");
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.RespondWithJson(new InvokedResponse() { Name = "Look, I was invoked!" });
+
+            var invokedResponse = await task;
+            invokedResponse.Name.Should().Be("Look, I was invoked!");
+        }
+
+        [Fact]
+        public async Task InvokeMethodAsync_CanInvokeMethodWithReturnTypeAndData_EmptyResponseReturnsNull()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokeRequest, InvokedResponse>("test", "test", new InvokeRequest() { RequestParameter = "Hello " });
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.Respond(new HttpResponseMessage(HttpStatusCode.OK));
+
+            var invokedResponse = await task;
+            invokedResponse.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task InvokeMethodAsync_CanInvokeMethodWithReturnTypeAndData_ThrowsExceptionForNonSuccess()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokeRequest, InvokedResponse>("test", "test", new InvokeRequest() { RequestParameter = "Hello " });
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.Respond(new HttpResponseMessage(HttpStatusCode.NotAcceptable));
+
+            await FluentActions.Awaiting(async () => await task).Should().ThrowAsync<HttpRequestException>();
+        }
+
+        [Fact]
+        public async Task InvokeMethodAsync_CanInvokeMethodWithReturnTypeNoData()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokedResponse>("test", "test");
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.RespondWithJson(new InvokedResponse() { Name = "Look, I was invoked!" });
+
+            var invokedResponse = await task;
+            invokedResponse.Name.Should().Be("Look, I was invoked!");
+        }
+
+        [Fact]
+        public async Task InvokeMethodAsync_CanInvokeMethodWithReturnTypeNoData_ThrowsExceptionNonSuccess()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokedResponse>("test", "test");
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.Respond(new HttpResponseMessage(HttpStatusCode.NotAcceptable));
+
+            await FluentActions.Awaiting(async () => await task).Should().ThrowAsync<HttpRequestException>();
+        }
+
+        [Fact]
+        public Task InvokeMethodAsync_CanInvokeMethodWithNoReturnTypeAndData()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokeRequest>("test", "test", new InvokeRequest() { RequestParameter = "Hello " });
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.RespondWithJson(new InvokedResponse() { Name = "Look, I was invoked!" });
+
+            FluentActions.Awaiting(async () => await task).Should().NotThrow();
+
+            return Task.FromResult(string.Empty);
+        }
+
+        [Fact]
+        public Task InvokeMethodAsync_CanInvokeMethodWithNoReturnTypeAndData_ThrowsErrorNonSuccess()
+        {
+            var httpClient = new TestHttpClient();
+            var invokeClient = new InvokeHttpClient(httpClient, new JsonSerializerOptions());
+
+            var task = invokeClient.InvokeMethodAsync<InvokeRequest>("test", "test", new InvokeRequest() { RequestParameter = "Hello " });
+
+            httpClient.Requests.TryDequeue(out var entry).Should().BeTrue();
+            entry.Request.RequestUri.ToString().Should().Be(GetInvokeUrl(3500, "test", "test"));
+
+            entry.Respond(new HttpResponseMessage(HttpStatusCode.NotAcceptable));
+
+            FluentActions.Awaiting(async () => await task).Should().ThrowAsync<HttpRequestException>();
+
+            return Task.FromResult(string.Empty);
+        }
+
+        private static string GetInvokeUrl(int port, string serviceName, string methodName)
+        {
+            return $"http://localhost:{port}/v1.0/invoke/{serviceName}/method/{methodName}";
+        }
+
+        private class InvokeRequest
+        {
+            public string RequestParameter { get; set; }
+        }
+
+        private class InvokedResponse
+        {
+            public string Name { get; set; }
+        }
+    }
+}
