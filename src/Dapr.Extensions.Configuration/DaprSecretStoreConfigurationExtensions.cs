6b50c5ad054caa5400d5c865a7a9a28d63b11c5e
cmendible <cmendible@gmail.com>
2020-03-02 21:05:37 +0100

    Implementing Dapr Secret Store configuration provider

---
+++ src/Dapr.Extensions.Configuration/DaprSecretStoreConfigurationExtensions.cs
@@ -0,0 +1,56 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Dapr.Client;
+using Microsoft.Extensions.Configuration;
+using Dapr.Extensions.Configuration.DaprSecretStore;
+
+namespace Dapr.Extensions.Configuration
+{
+    /// <summary>
+    /// Extension methods for registering <see cref="DaprSecretStoreConfigurationProvider"/> with <see cref="IConfigurationBuilder"/>.
+    /// </summary>
+    public static class DaprSecretStoreConfigurationExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="IConfigurationProvider"/> that reads configuration values from the Dapr Secret Store.
+        /// </summary>
+        /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="store">Dapr secret store name.</param>
+        /// <param name="secretDescriptors">The secrets to retrieve.</param>
+        /// <param name="client">The Dapr client</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        public static IConfigurationBuilder AddDaprSecretStore(
+            this IConfigurationBuilder configurationBuilder,
+            string store,
+            IEnumerable<DaprSecretDescriptor> secretDescriptors,
+            DaprClient client)
+        {
+            ArgumentVerifier.ThrowIfNullOrEmpty(store, nameof(store));
+            ArgumentVerifier.ThrowIfNull(secretDescriptors, nameof(secretDescriptors));
+            ArgumentVerifier.ThrowIfNull(client, nameof(client));
+
+            configurationBuilder.Add(new DaprSecretStoreConfigurationSource()
+            {
+                Store = store,
+                SecretDescriptors = secretDescriptors,
+                Client = client
+            });
+
+            return configurationBuilder;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="IConfigurationProvider"/> that reads configuration values from the command line.
+        /// </summary>
+        /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="configureSource">Configures the source.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        public static IConfigurationBuilder AddDaprSecretStore(this IConfigurationBuilder configurationBuilder, Action<DaprSecretStoreConfigurationSource> configureSource)
+            => configurationBuilder.Add(configureSource);
+    }
+}
---
+++ src/Dapr.Extensions.Configuration/DaprSecretStoreConfigurationSource.cs
@@ -0,0 +1,38 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+using System.Collections.Generic;
+using Dapr.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace Dapr.Extensions.Configuration.DaprSecretStore
+{
+    /// <summary>
+    /// Represents Dapr Secret Store as an <see cref="IConfigurationSource"/>.
+    /// </summary>
+    public class DaprSecretStoreConfigurationSource : IConfigurationSource
+    {
+        /// <summary>
+        /// Gets or sets the store name.
+        /// </summary>
+        public string Store { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret names.
+        /// </summary>
+        public IEnumerable<DaprSecretDescriptor> SecretDescriptors { get; set; }
+
+        /// <summary>
+        /// Gets or sets the http client.
+        /// </summary>
+        public DaprClient Client { get; set; }
+
+        /// <inheritdoc />
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new DaprSecretStoreConfigurationProvider(Store, SecretDescriptors, Client);
+        }
+    }
+}
