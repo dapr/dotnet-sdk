5b246c866593b2498590df6d8fd0dcfb6d329e8f
Christian Kaps <307006+akkie@users.noreply.github.com>
2020-07-25 18:20:34 +0200

    Include the appcallback.proto into the Dapr.Client package (#349)
    * Include the appcallback.proto into the Dapr.Client package
    
    To implement service invocation for a gRPC API, a user must implement the AppCallback service on the callee site. Currently this must be done by integrating the `appcallpack.proto` file as also the depending `common.proto` file into the gRPC service application. The `Dapr.Client` package contains already the same `common.proto` file to generate the client classes. This results in a CS0433 error, because the `Dapr.Client` package and the generated AppCallback service will contain a `Dapr.Client.Autogen.Grpc.v1` namespace with the exact same classes.
    
    This pull requests integrates the `appcallpack.proto` into the client package. With this fix the user does not need to integrate the proto files by itself.
    
    See: https://gitter.im/Dapr/community?at=5f14b7e98a9a0a08cbab5d53
    
    * Remove specific names

---
+++ test/Shared/AppCallbackClient.cs
@@ -0,0 +1,73 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace Dapr
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Dapr.Client.Autogen.Grpc.v1;
+    using Grpc.Core;
+    using Grpc.Core.Testing;
+    using Grpc.Core.Utils;
+    using AppCallbackBase = AppCallback.Autogen.Grpc.v1.AppCallback.AppCallbackBase;
+
+    // This client will forward requests to the AppCallback service implementation which then responds to the request
+    public class AppCallbackClient : HttpClient
+    {
+        public AppCallbackClient(AppCallbackBase callbackService)
+            : this(new TestHttpClientHandler(callbackService))
+        {
+        }
+
+        private AppCallbackClient(TestHttpClientHandler handler)
+            : base(handler)
+        {
+        }
+
+        private class TestHttpClientHandler : HttpMessageHandler
+        {
+            private readonly AppCallbackBase callbackService;
+
+            public TestHttpClientHandler(AppCallbackBase callbackService)
+            {
+                this.callbackService = callbackService;
+            }
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequest, CancellationToken cancellationToken)
+            {
+                var metadata = new Metadata();
+                foreach (var (key, value) in httpRequest.Headers)
+                {
+                    metadata.Add(key, string.Join(",", value.ToArray()));
+                }
+
+                var context = TestServerCallContext.Create(
+                    method: httpRequest.Method.Method,
+                    host: httpRequest.RequestUri.Host,
+                    deadline: DateTime.UtcNow.AddHours(1),
+                    requestHeaders: metadata,
+                    cancellationToken: cancellationToken,
+                    peer: "127.0.0.1",
+                    authContext: null,
+                    contextPropagationToken: null,
+                    writeHeadersFunc: _ => TaskUtils.CompletedTask,
+                    writeOptionsGetter: () => new WriteOptions(),
+                    writeOptionsSetter: writeOptions => {});
+
+                var grpcRequest = await GrpcUtils.GetRequestFromRequestMessageAsync<InvokeServiceRequest>(httpRequest);
+                var grpcResponse = await this.callbackService.OnInvoke(grpcRequest.Message, context);
+
+                var streamContent = await GrpcUtils.CreateResponseContent(grpcResponse);
+                var httpResponse = GrpcUtils.CreateResponse(HttpStatusCode.OK, streamContent);
+
+                return httpResponse;
+            }
+        }
+    }
+}
