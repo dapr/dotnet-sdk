50809c46c1b5fc4d89025bd6ae3566b8d97e6d56
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2020-08-20 10:04:09 +0800

    disable TLS for grpc server

--- samples/AspNetCore/GrpcServiceSample/Program.cs
+++ samples/AspNetCore/GrpcServiceSample/Program.cs
@@ -1,9 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace GrpcServiceSample
@@ -21,6 +22,13 @@ namespace GrpcServiceSample
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(5000, o => o.Protocols =
+                            HttpProtocols.Http2);
+                    });
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
