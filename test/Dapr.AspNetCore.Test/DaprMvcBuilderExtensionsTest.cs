b3aef53d793cae82c5dc3c3254d20c237c8f7a5c
Edwin van Wijk <edwinvw@gmail.com>
2020-09-28 19:24:45 +0200

    Fix use of marker services in DI registration (#403)
    * Fix for issue #390
    
    * Fix unit-test naming
    
    Co-authored-by: vinayada1 <28875764+vinayada1@users.noreply.github.com>

--- test/Dapr.AspNetCore.Test/DaprMvcBuilderExtensionsTest.cs
+++ test/Dapr.AspNetCore.Test/DaprMvcBuilderExtensionsTest.cs
@@ -44,5 +44,32 @@ namespace Dapr.AspNetCore.Test
 
             Assert.True(daprClient.JsonSerializerOptions.PropertyNameCaseInsensitive);
         }
+
+        [Fact]
+        public void AddDapr_RegistersDaprOnlyOnce()
+        {
+            var services = new ServiceCollection();
+
+            var clientBuilder = new Action<DaprClientBuilder>(
+                builder => builder.UseJsonSerializationOptions(
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = false
+                    }
+                )
+            );
+
+            // register with JsonSerializerOptions.PropertyNameCaseInsensitive = false
+            services.AddControllers().AddDapr(clientBuilder);
+
+            // register with JsonSerializerOptions.PropertyNameCaseInsensitive = true (default)
+            services.AddControllers().AddDapr();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            DaprClientGrpc daprClient = serviceProvider.GetService<DaprClient>() as DaprClientGrpc;
+
+            Assert.False(daprClient.JsonSerializerOptions.PropertyNameCaseInsensitive);
+        }
     }
 }
---
+++ test/Dapr.AspNetCore.Test/DaprServiceCollectionExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using Dapr.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Dapr.AspNetCore.Test
+{
+    public class DaprServiceCollectionExtensionsTest
+    {
+        [Fact]
+        public void AddDaprClient_RegistersDaprClientOnlyOnce()
+        {
+            var services = new ServiceCollection();
+
+            var clientBuilder = new Action<DaprClientBuilder>(
+                builder => builder.UseJsonSerializationOptions(
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = false
+                    }
+                )
+            );
+
+            // register with JsonSerializerOptions.PropertyNameCaseInsensitive = true (default)
+            services.AddDaprClient();
+
+            // register with PropertyNameCaseInsensitive = false
+            services.AddDaprClient(clientBuilder);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            DaprClientGrpc daprClient = serviceProvider.GetService<DaprClient>() as DaprClientGrpc;
+
+            Assert.True(daprClient.JsonSerializerOptions.PropertyNameCaseInsensitive);
+        }
+    }
+}
