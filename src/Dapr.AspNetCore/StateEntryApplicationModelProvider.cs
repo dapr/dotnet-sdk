5f38e63afca12f477fae504d7721ee78d18b3512
Daniel Stoyanoff <dannystoyanoff@gmail.com>
2020-12-05 01:08:20 +0200

    #496 - add a check if there is no BindingInfo for action parameters (#497)
    * #496 - add a check if there is no BindingInfo for acton parameters
    
    * #496 - replace if with elseif
    
    Co-authored-by: Daniel <daniel.s@appgr8.com>

--- src/Dapr.AspNetCore/StateEntryApplicationModelProvider.cs
+++ src/Dapr.AspNetCore/StateEntryApplicationModelProvider.cs
@@ -42,7 +42,11 @@ namespace Dapr.AspNetCore
                 {
                     foreach (var parameter in action.Parameters)
                     {
-                        if (parameter.BindingInfo.BindingSource.Id == "state")
+                        if (parameter.BindingInfo == null)
+                        {
+                            // Not bindable.
+                        }
+                        else if (parameter.BindingInfo.BindingSource.Id == "state")
                         {
                             // Already configured, don't overwrite in case the user customized it.
                         }
