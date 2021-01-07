74e5773ea33e45811e0896b1b86290d2f0e58788
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-01-07 11:12:17 -0800

    add negative UT

--- test/Dapr.Actors.Test/ActorProxyTests.cs
+++ test/Dapr.Actors.Test/ActorProxyTests.cs
@@ -5,9 +5,11 @@
 
 namespace Dapr.Actors.Client
 {
+    using System;
     using Dapr.Actors.Builder;
     using Dapr.Actors.Client;
     using Dapr.Actors.Test;
+    using FluentAssertions;
     using Xunit;
 
     /// <summary>
@@ -19,13 +21,21 @@ namespace Dapr.Actors.Client
         /// Tests Proxy Creation.
         /// </summary>
         [Fact]
-        public void TestCreateActorProxy()
+        public void TestCreateActorProxy_Success()
         {
             var actorId = new ActorId("abc");
             var proxy1 = ActorProxy.Create(actorId, "TestActor");
             Assert.NotNull(proxy1);
             var proxy2 = ActorProxy.Create(actorId, typeof(ITestActor), "TestActor");
             Assert.NotNull(proxy2);
+        }
+
+        [Fact]
+        public void TestCreateActorProxy_NonSuccess()
+        {
+            var actorId = new ActorId("abc");
+            Action action = () =>  ActorProxy.Create(actorId, typeof(ActorId), "TestActor");
+            action.Should().Throw<ArgumentException>();
         }
     }
 }
