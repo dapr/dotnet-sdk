7f9d23cdf50b9bc3ac52444810244cc76dae06c0
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-01-29 18:40:23 -0800

    fixes for getting the samples to work (#570)
    * fixes for getting the samples to work
    
    * add ut

--- samples/Client/DaprClient/InvokeServiceGrpcExample.cs
+++ samples/Client/DaprClient/InvokeServiceGrpcExample.cs
@@ -20,21 +20,21 @@ namespace Samples.Client
         {
             using var client = new DaprClientBuilder().Build();
 
-            Console.WriteLine("Invoking grpc balance");
-            var request = new GetAccountRequest() { Id = "17", };
-            var account = await client.InvokeMethodGrpcAsync<GetAccountRequest, Account>("grpcsample", "getaccount", request, cancellationToken);
-            Console.WriteLine($"Received grpc balance {account.Balance}");
-
             Console.WriteLine("Invoking grpc deposit");
-            var data = new GrpcServiceSample.Generated.Transaction() { Id = "17", Amount = 99 };
-            account = await client.InvokeMethodGrpcAsync<Transaction, Account>("grpcsample", "deposit", data, cancellationToken);
+            var deposit = new GrpcServiceSample.Generated.Transaction() { Id = "17", Amount = 99 };
+            var account = await client.InvokeMethodGrpcAsync<GrpcServiceSample.Generated.Transaction, Account>("grpcsample", "deposit", deposit, cancellationToken);
             Console.WriteLine("Returned: id:{0} | Balance:{1}", account.Id, account.Balance);
             Console.WriteLine("Completed grpc deposit");
 
             Console.WriteLine("Invoking grpc withdraw");
-            var withdraw = new Transaction() { Id = "17", Amount = 10, };
-            await client.InvokeMethodGrpcAsync("grpcsample", "withdraw", data, cancellationToken);
+            var withdraw = new GrpcServiceSample.Generated.Transaction() { Id = "17", Amount = 10, };
+            await client.InvokeMethodGrpcAsync("grpcsample", "withdraw", withdraw, cancellationToken);
             Console.WriteLine("Completed grpc withdraw");
+
+            Console.WriteLine("Invoking grpc balance");
+            var request = new GetAccountRequest() { Id = "17", };
+            account = await client.InvokeMethodGrpcAsync<GetAccountRequest, Account>("grpcsample", "getaccount", request, cancellationToken);
+            Console.WriteLine($"Received grpc balance {account.Balance}");
         }
     }
 }
