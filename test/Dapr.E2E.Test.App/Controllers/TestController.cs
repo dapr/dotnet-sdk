40e5b62c876c31bfb7d921c9db42a4c5656b081e
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-02-19 16:24:52 -0800

    e2e tests

---
+++ test/Dapr.E2E.Test.App/Controllers/TestController.cs
@@ -0,0 +1,118 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace Dapr.AspNetCore.E2ETests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Dapr;
+    using Dapr.Client;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Sample showing Dapr integration with controller.
+    /// </summary>
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// SampleController Constructor with logger injection
+        /// </summary>
+        /// <param name="logger"></param>
+        public TestController(ILogger<TestController> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// State store name.
+        /// </summary>
+        public const string StoreName = "statestore";
+        private readonly ILogger<TestController> logger;
+
+        /// <summary>
+        /// Gets the account information as specified by the id.
+        /// </summary>
+        /// <param name="account">Account information for the id from Dapr state store.</param>
+        /// <returns>Account information.</returns>
+        [HttpGet("{account}")]
+        public ActionResult<Account> Get([FromState(StoreName)] StateEntry<Account> account)
+        {
+            if (account.Value is null)
+            {
+                return this.NotFound();
+            }
+
+            return account.Value;
+        }
+
+        /// <summary>
+        /// Invoke Method.
+        /// </summary>
+        /// <param name="name">string input to echo.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        ///  "pubsub", the first parameter into the Topic attribute, is name of the default pub/sub configured by the Dapr CLI.
+        [HttpGet("hello")]
+        public async Task<ActionResult<string>> Hello(string name)
+        {
+            return ($"Hello {name}!");
+        }
+
+        /// <summary>
+        /// Method for depositing to account as specified in transaction.
+        /// </summary>
+        /// <param name="transaction">Transaction info.</param>
+        /// <param name="daprClient">State client to interact with Dapr runtime.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        ///  "pubsub", the first parameter into the Topic attribute, is name of the default pub/sub configured by the Dapr CLI.
+        [Topic("pubsub", "deposit")]
+        [HttpPost("deposit")]
+        public async Task<ActionResult<Account>> Deposit(Transaction transaction, [FromServices] DaprClient daprClient)
+        {
+            logger.LogDebug("Enter deposit");
+            var state = await daprClient.GetStateEntryAsync<Account>(StoreName, transaction.Id);
+            state.Value ??= new Account() { Id = transaction.Id, };
+            state.Value.Balance += transaction.Amount;
+            await state.SaveAsync();
+            return state.Value;
+        }
+
+        /// <summary>
+        /// Method for withdrawing from account as specified in transaction.
+        /// </summary>
+        /// <param name="transaction">Transaction info.</param>
+        /// <param name="daprClient">State client to interact with Dapr runtime.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        ///  "pubsub", the first parameter into the Topic attribute, is name of the default pub/sub configured by the Dapr CLI.
+        [Topic("pubsub", "withdraw")]
+        [HttpPost("withdraw")]
+        public async Task<ActionResult<Account>> Withdraw(Transaction transaction, [FromServices] DaprClient daprClient)
+        {
+            logger.LogDebug("Enter withdraw");
+            var state = await daprClient.GetStateEntryAsync<Account>(StoreName, transaction.Id);
+
+            if (state.Value == null)
+            {
+                return this.NotFound();
+            }
+
+            state.Value.Balance -= transaction.Amount;
+            await state.SaveAsync();
+            return state.Value;
+        }
+
+        /// <summary>
+        /// Method for returning a BadRequest result which will cause Dapr sidecar to throw an RpcException
+        [HttpPost("throwException")]
+        public async Task<ActionResult<Account>> ThrowException(Transaction transaction, [FromServices] DaprClient daprClient)
+        {
+            Console.WriteLine("Enter ThrowException");
+            var task = Task.Delay(10);
+            await task;
+            return BadRequest(new { statusCode = 400, message = "bad request" });
+        }
+    }
+}
