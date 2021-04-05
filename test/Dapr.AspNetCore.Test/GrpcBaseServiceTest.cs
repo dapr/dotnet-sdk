b59e506066650278228a404b607b209d9f60baff
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2021-04-05 22:12:13 +0800

    add test for GrpcBaseService (not completed)

---
+++ test/Dapr.AspNetCore.Test/GrpcBaseServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Dapr.AspNetCore.Test
+{
+    public class GrpcBaseServiceTest
+    {
+        [Fact]
+        public void GrpcBaseService_HappyPath()
+        {
+            
+        }
+    }
+}
---
+++ test/Dapr.AspNetCore.Test/Targets/HappyGrpcService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dapr.AspNetCore.Test.Generated;
+using Dapr.Client;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Dapr.AspNetCore.Test
+{
+    public class HappyGrpcService : GrpcBaseService
+    {
+        public HappyGrpcService(DaprClient daprClient, ILogger logger) : base(daprClient, logger)
+        {
+        }
+
+        [GrpcInvoke(typeof(AccountRequest))]
+        public Task<Account> GetAccount(AccountRequest model, ServerCallContext context)
+        {
+            return Task.FromResult(new Account { Id = "test", Balance = 123 });
+        }
+
+        [GrpcInvoke(typeof(AccountRequest), "rich")]
+        public Task<Transaction> LetUsRich(AccountRequest model, ServerCallContext context)
+        {
+            return Task.FromResult(new Transaction { Id = "test", Amount = 100000 });
+        }
+    }
+}
