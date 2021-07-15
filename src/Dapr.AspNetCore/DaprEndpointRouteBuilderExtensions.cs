d610ca95ad0c785b9414335a56091a5957028d3a
Sapinder <sapinderpal@outlook.com>
2021-07-15 22:44:07 +0530

    Added support for processing raw messages.

--- src/Dapr.AspNetCore/DaprEndpointRouteBuilderExtensions.cs
+++ src/Dapr.AspNetCore/DaprEndpointRouteBuilderExtensions.cs
@@ -5,12 +5,11 @@
 
 namespace Microsoft.AspNetCore.Builder
 {
-    using System;
+    using System.Collections;
     using System.Linq;
     using System.Text.Json;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Dapr;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Routing;
     using Microsoft.AspNetCore.Routing.Patterns;
     using Microsoft.Extensions.DependencyInjection;
@@ -76,6 +75,68 @@ namespace Microsoft.AspNetCore.Builder
 
                 writer.WriteEndArray();
                 await writer.FlushAsync();
+            });
+        }
+
+        /// <summary>
+        /// Maps an endpoint that will respond to requests to <c>/dapr/subscribe</c> from the
+        /// Dapr runtime.
+        /// </summary>
+        /// <param name="endpoints">The <see cref="IEndpointRouteBuilder" />.</param>
+        /// <param name="options">Configuration options</param>
+        /// <returns>The <see cref="IEndpointConventionBuilder" />.</returns>
+        /// <seealso cref="MapSubscribeHandler(IEndpointRouteBuilder)"/>
+        public static IEndpointConventionBuilder MapSubscribeHandler(this IEndpointRouteBuilder endpoints, SubscribeOptions options)
+        {
+            if (endpoints is null)
+            {
+                throw new System.ArgumentNullException(nameof(endpoints));
+            }
+
+            return endpoints.MapGet("dapr/subscribe", async context =>
+            {
+                var dataSource = context.RequestServices.GetRequiredService<EndpointDataSource>();
+                var entries = dataSource.Endpoints
+                    .OfType<RouteEndpoint>()
+                    .Where(e => e.Metadata.GetMetadata<ITopicMetadata>()?.Name != null)   // only endpoints which have  TopicAttribute with not null Name.
+                    .Distinct()
+                    .Select(e => (e.Metadata.GetMetadata<ITopicMetadata>().PubsubName, e.Metadata.GetMetadata<ITopicMetadata>().Name, e.Metadata.GetMetadata<IRawTopicMetadata>()?.EnableRawPayload, e.RoutePattern));
+
+
+                var logger = context.RequestServices.GetService<ILoggerFactory>().CreateLogger("DaprTopicSubscription");
+
+                ArrayList subscriptions = new();
+                foreach (var entry in entries)
+                {
+                    // only return topics which have routes without parameters.
+                    if (entry.RoutePattern.Parameters.Count > 0)
+                    {
+                        if (logger != null)
+                        {
+                            logger.LogError("Topic subscription doesn't support route with parameters. Subscription for topic {name} is removed.", entry.Name);
+                        }
+
+                        continue;
+                    }
+
+                    var route = string.Join("/",
+                        entry.RoutePattern.PathSegments
+                        .Select(segment => string.Concat(segment.Parts.Cast<RoutePatternLiteralPart>()
+                        .Select(part => part.Content))));
+
+                    subscriptions.Add(new
+                    {
+                        topic = entry.Name,
+                        pubsubName = entry.PubsubName,
+                        route,
+                        metadata = new
+                        {
+                            rawPayload = (entry.EnableRawPayload == true) ? "true" : (entry.EnableRawPayload == false) ? "false" : options.EnableRawPayload.ToString().ToLower()
+                        }
+                    }); ;
+                }
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(subscriptions));
             });
         }
     }
