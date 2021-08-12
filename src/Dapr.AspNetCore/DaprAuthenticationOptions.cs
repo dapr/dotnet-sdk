9fe5485cf0a315f80116c785e4117c66d09804d7
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-08-12 03:55:59 +0300

    Adds Dapr authentication handler (#688)
    * Adds Dapr authentication handler
    
    - This includes functionality which simplifies securing endpoints using Dapr API Token authentication
    - Fixes #682
    
    * Remove Dapr authentication handler from examples.
    
    * Don't resolve Dapr API token on every request, set it using delegate when configuring Dapr authentication instead.
    
    * Adds integration tests for Dapr API token
    
    Co-authored-by: Yaron Schneider <yaronsc@microsoft.com>

---
+++ src/Dapr.AspNetCore/DaprAuthenticationOptions.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Dapr.AspNetCore
+{
+    /// <summary>
+    /// Options class provides information needed to control Dapr Authentication handler behavior.
+    /// See https://docs.dapr.io/operations/security/api-token/ for more information about API token authentication in Dapr.
+    /// </summary>
+    public class DaprAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        internal const string DefaultScheme = "Dapr";
+        internal string Scheme { get; } = DefaultScheme;
+
+        /// <summary>
+        /// Gets or sets the Dapr API token.
+        /// By default, the token will be read from the DAPR_API_TOKEN environment variable.
+        /// </summary>
+        public string Token { get; set; } = DaprDefaults.GetDefaultApiToken();
+    }
+}
