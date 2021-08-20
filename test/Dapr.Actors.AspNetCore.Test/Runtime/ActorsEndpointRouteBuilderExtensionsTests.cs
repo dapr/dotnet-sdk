f337af18840e9c0eb52efb76adec25cc59df2705
dodd <358683537@qq.com>
2021-08-20 09:34:18 +0800

    feat: add MapDaprConfigEndpoint test

---
+++ test/Dapr.Actors.AspNetCore.Test/Runtime/ActorsEndpointRouteBuilderExtensionsTests.cs
@@ -0,0 +1,105 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Dapr.Actors.Runtime
+{
+    public class ActorsEndpointRouteBuilderExtensionsTests
+    {
+        [Fact]
+        public async Task MapActorsHandlers_MapDaprConfigEndpoint()
+        {
+            using var host = CreateHost<ActorsStartup>(options =>
+            {
+                options.Actors.RegisterActor<TestActor>();
+            });
+            var server = host.GetTestServer();
+
+            var httpClient = server.CreateClient();
+            var response = await httpClient.GetAsync("/dapr/config");
+
+            var text = await response.Content.ReadAsStringAsync();
+            Assert.Equal(@"{""entities"":[""TestActor""],""reentrancy"":{""enabled"":false}}", text);
+        }
+
+        private static IHost CreateHost<TStartup>(Action<ActorRuntimeOptions> configure) where TStartup : class
+        {
+            var builder = Host
+                .CreateDefaultBuilder()
+                .ConfigureLogging(b =>
+                {
+                    // shhhh
+                    b.SetMinimumLevel(LogLevel.None);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<TStartup>();
+                    webBuilder.UseTestServer();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddActors(configure);
+                });
+            var host = builder.Build();
+            try
+            {
+                host.Start();
+            }
+            catch
+            {
+                host.Dispose();
+                throw;
+            }
+
+            return host;
+        }
+
+        private class ActorsStartup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddActors(default);
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
+                {
+                    //endpoints.MapHealthChecks("/healthz", new HealthCheckOptions()
+                    //{
+                    //    // Write something different so we know this one is called.
+                    //    ResponseWriter = async (httpContext, report) =>
+                    //    {
+                    //        await httpContext.Response.WriteAsync(
+                    //            report.Status == HealthStatus.Healthy ?
+                    //            "Ice Cold, Solid Gold!" :
+                    //            "Oh Noes!");
+                    //    },
+                    //});
+                    endpoints.MapActorsHandlers();
+                });
+            }
+        }
+
+        private interface ITestActor : IActor
+        {
+        }
+
+        private class TestActor : Actor, ITestActor
+        {
+            public TestActor(ActorHost host) : base(host) { }
+        }
+    }
+}
