52b7e2facf6e782996dbce3e48d7bcbf0f41cd68
saberwang <saberwang@hotmail.com>
2022-07-30 12:13:02 +0800

    style Signed-off-by: saberwang saberwang@hotmail.com

--- test/Dapr.Client.Test/DaprClientTest.InvokeMethodGrpcAsync.cs
+++ test/Dapr.Client.Test/DaprClientTest.InvokeMethodGrpcAsync.cs
@@ -12,7 +12,6 @@
 // ------------------------------------------------------------------------
 
 using System;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapr.Client.Autogen.Grpc.v1;
