d7fd160583db01835b62e426cb5a0a4937d65ac2
Phillip Hoff <phillip@orst.edu>
2023-10-10 08:59:36 -0700

    Sketch test for cancellation tokens.
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

--- src/Dapr.Actors.Generators/GenerateActorClientAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Dapr.Actors;
-
-/// <summary>
-/// Indicates an actor interface should have a strongly-typed client generated for it.
-/// </summary>
-[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
-public sealed class GenerateActorClientAttribute : Attribute
-{
-}
