075c9077bdcfc8873c49f60a871a748427a7f4d8
MregXN <mregxn@gmail.com>
2023-12-09 18:46:53 +0800

    Add workflow sample task chaining
    Signed-off-by: MregXN <mregxn@gmail.com>

---
+++ examples/Workflow/WorkflowTaskChaining/Activities/Step1.cs
@@ -0,0 +1,13 @@
+﻿using Dapr.Workflow;
+
+namespace WorkflowTaskChianing.Activities
+{
+    public class Step1 : WorkflowActivity<int, int>
+    {
+        public override Task<int> RunAsync(WorkflowActivityContext context, int input)
+        {
+            Console.WriteLine($"Step 1: Received input: {input}.");
+            return Task.FromResult<int>(input + 1);
+        }
+    }
+}
---
+++ examples/Workflow/WorkflowTaskChaining/Activities/Step2.cs
@@ -0,0 +1,13 @@
+﻿using Dapr.Workflow;
+
+namespace WorkflowTaskChianing.Activities
+{
+    public class Step2 : WorkflowActivity<int, int>
+    {
+        public override Task<int> RunAsync(WorkflowActivityContext context, int input)
+        {
+            Console.WriteLine($"Step 2: Received input: {input}.");
+            return Task.FromResult<int>(input * 2);
+        }
+    }
+}
