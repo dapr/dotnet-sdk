2c293fee8b9042b5a9e3a4dbca3949f7682bae01
MregXN <mregxn@gmail.com>
2023-12-10 20:36:29 +0800

    Add workflow sample monitor
    Signed-off-by: MregXN <mregxn@gmail.com>

---
+++ examples/Workflow/WorkflowMonitor/Activities/CheckStatus.cs
@@ -0,0 +1,14 @@
+﻿using Dapr.Workflow;
+
+namespace WorkflowMonitor.Activities
+{
+    public class CheckStatus : WorkflowActivity<bool, string>
+    {
+        static List<string> status = new List<string>() { "healthy", "unhealthy" };
+        Random random = new Random();
+        public override Task<string> RunAsync(WorkflowActivityContext context, bool input)
+        {
+            return Task.FromResult<string>(status[random.Next(status.Count)]);
+        }
+    }
+}
