b473d794ebdbeb0ef2ae40958a15ccae567f0e43
Elena Kolevska <elena@kolevska.com>
2024-03-12 15:30:52 +0000

    Adds a timeout for the grpc client
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

---
+++ src/Dapr.Client/TimeoutCallInvoker.cs
@@ -0,0 +1,124 @@
+namespace Dapr.Client
+{
+    using System;
+    using Grpc.Core;
+
+    /// <summary>
+    /// gRPC interceptor which adds the required deadline for Dapr gRPC calls.
+    /// </summary>
+    public class TimeoutCallInvoker : CallInvoker
+    {
+        private readonly CallInvoker _invoker;
+        private readonly TimeSpan _timeout;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeoutCallInvoker"/> class.
+        /// </summary>
+        /// <param name="invoker">The underlying call invoker to wrap.</param>
+        /// <param name="timeout">The timeout to apply to all calls.</param>
+        public TimeoutCallInvoker(CallInvoker invoker, TimeSpan timeout)
+        {
+            _invoker = invoker;
+            _timeout = timeout;
+        }
+
+        /// <summary>
+        /// Initiates a blocking unary call with a timeout.
+        /// </summary>
+        /// <param name="method">The method to call.</param>
+        /// <param name="host">The host where the method is located.</param>
+        /// <param name="options">The call options.</param>
+        /// <param name="request">The request message.</param>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <typeparam name="TResponse">The type of the response.</typeparam>
+        /// <returns>The response from the call.</returns>
+        public override TResponse BlockingUnaryCall<TRequest, TResponse>(
+        Method<TRequest, TResponse> method,
+            string host,
+            CallOptions options,
+            TRequest request)
+        {
+            var callOptionsWithDeadline = options.WithDeadline(DateTime.UtcNow.Add(_timeout));
+            return _invoker.BlockingUnaryCall(method, host, callOptionsWithDeadline, request);
+        }
+
+        /// <summary>
+        /// Initiates an async unary call with a timeout.
+        /// </summary>
+        /// <param name="method">The method to call.</param>
+        /// <param name="host">The host where the method is located.</param>
+        /// <param name="options">The call options.</param>
+        /// <param name="request">The request message.</param>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <typeparam name="TResponse">The type of the response.</typeparam>
+        /// <returns>The response from the call.</returns>
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+            Method<TRequest, TResponse> method,
+            string host,
+            CallOptions options,
+            TRequest request)
+        {
+            var callOptionsWithDeadline = options.WithDeadline(DateTime.UtcNow.Add(_timeout));
+            return _invoker.AsyncUnaryCall(method, host, callOptionsWithDeadline, request);
+        }
+
+        /// <summary>
+        /// Initiates an async client streaming call with a timeout.
+        /// </summary>
+        /// <param name="method">The method to call.</param>
+        /// <param name="host">The host where the method is located.</param>
+        /// <param name="options">The call options.</param>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <typeparam name="TResponse">The type of the response.</typeparam>
+        /// <returns>The response from the call.</returns>
+        public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall<TRequest, TResponse>(
+            Method<TRequest, TResponse> method,
+            string host,
+            CallOptions options)
+        {
+            var callOptionsWithDeadline = options.WithDeadline(DateTime.UtcNow.Add(_timeout));
+            return _invoker.AsyncClientStreamingCall(method, host, callOptionsWithDeadline);
+        }
+
+        /// <summary>
+        /// Initiates an async server streaming call with a timeout.
+        /// </summary>
+        /// <param name="method">The method to call.</param>
+        /// <param name="host">The host where the method is located.</param>
+        /// <param name="options">The call options.</param>
+        /// <param name="request">The request message.</param>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <typeparam name="TResponse">The type of the response.</typeparam>
+        /// <returns>The response from the call.</returns>
+        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(
+            Method<TRequest, TResponse> method,
+            string host,
+            CallOptions options,
+            TRequest request)
+        {
+            var callOptionsWithDeadline = options.WithDeadline(DateTime.UtcNow.Add(_timeout));
+            return _invoker.AsyncServerStreamingCall(method, host, callOptionsWithDeadline, request);
+        }
+
+        /// <summary>
+        /// Initiates an async duplex streaming call with a timeout.
+        /// </summary>
+        /// <param name="method">The method to call.</param>
+        /// <param name="host">The host where the method is located.</param>
+        /// <param name="options">The call options.</param>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <typeparam name="TResponse">The type of the response.</typeparam>
+        /// <returns>The response from the call.</returns>
+        public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(
+            Method<TRequest, TResponse> method,
+            string host,
+            CallOptions options)
+        {
+            var callOptionsWithDeadline = options.WithDeadline(DateTime.UtcNow.Add(_timeout));
+            return _invoker.AsyncDuplexStreamingCall(method, host, callOptionsWithDeadline);
+        }
+
+        
+    }
+
+}
