665cc7939c0308bc6de089df2ea26625b863a9dc
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2024-04-29 23:16:43 +0200

    Added unit test for GetActorReference from null actore and actor proxy
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

---
+++ test/Dapr.Actors.Test/ActorReferenceTests.cs
@@ -0,0 +1,42 @@
+﻿using Dapr.Actors.Client;
+using Dapr.Actors.Test;
+using Xunit;
+
+namespace Dapr.Actors
+{
+    public class ActorReferenceTests
+    {
+        [Fact]
+        public void GetActorReference_WhenActorIsNull_ReturnsNull()
+        {
+            // Arrange
+            object actor = null;
+
+            // Act
+            var result = ActorReference.Get(actor);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetActorReference_FromActorProxy_ReturnsActorReference()
+        {
+            // Arrange
+            var expectedActorId = new ActorId("abc");
+            var expectedActorType = "TestActor";
+            var proxy = ActorProxy.Create(expectedActorId, typeof(ITestActor), expectedActorType);
+
+            // Act
+            var actorReference = ActorReference.Get(proxy);
+
+            // Assert
+            Assert.NotNull(actorReference);
+            Assert.Equal(expectedActorId, actorReference.ActorId);
+            Assert.Equal(expectedActorType, actorReference.ActorType);
+        }
+
+
+
+    }
+}
