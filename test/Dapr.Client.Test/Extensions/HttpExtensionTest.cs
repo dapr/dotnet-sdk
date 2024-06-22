7431945c46325437adb9e0d1c656ce2c942185c8
Whit Waldo <whit.waldo@innovian.net>
2024-06-22 07:37:53 -0500

    Updated unit tests to match new extension purpose
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- test/Dapr.Client.Test/Extensions/HttpExtensionTest.cs
+++ test/Dapr.Client.Test/Extensions/HttpExtensionTest.cs
@@ -11,8 +11,8 @@ namespace Dapr.Client.Test.Extensions
         {
             const string uri = "https://localhost/mypath";
             var httpRq = new HttpRequestMessage(HttpMethod.Get, uri);
-            httpRq.AddQueryParameters(null);
-            Assert.Equal(uri, httpRq.RequestUri.AbsoluteUri);
+            var updatedUri = httpRq.RequestUri.AddQueryParameters(null);
+            Assert.Equal(uri, updatedUri.AbsoluteUri);
         }
 
         [Fact]
@@ -20,44 +20,44 @@ namespace Dapr.Client.Test.Extensions
         {
             const string uri = "https://localhost/mypath?a=0&b=1";
             var httpRq = new HttpRequestMessage(HttpMethod.Get, uri);
-            httpRq.AddQueryParameters(null);
-            Assert.Equal(uri, httpRq.RequestUri.AbsoluteUri);
+            var updatedUri = httpRq.RequestUri.AddQueryParameters(null);
+            Assert.Equal(uri, updatedUri.AbsoluteUri);
         }
 
         [Fact]
         public void AddQueryParameters_BuildsQueryString()
         {
             var httpRq = new HttpRequestMessage(HttpMethod.Get, "https://localhost/mypath?a=0");
-            httpRq.AddQueryParameters(new List<KeyValuePair<string,string>>
+            var updatedUri = httpRq.RequestUri.AddQueryParameters(new List<KeyValuePair<string,string>>
             {
                 new("test", "value")
             });
-            Assert.Equal("https://localhost/mypath?a=0&test=value", httpRq.RequestUri.AbsoluteUri);
+            Assert.Equal("https://localhost/mypath?a=0&test=value", updatedUri.AbsoluteUri);
         }
 
         [Fact]
         public void AddQueryParameters_BuildQueryStringWithDuplicateKeys()
         {
             var httpRq = new HttpRequestMessage(HttpMethod.Get, "https://localhost/mypath");
-            httpRq.AddQueryParameters(new List<KeyValuePair<string,string>>
+            var updatedUri = httpRq.RequestUri.AddQueryParameters(new List<KeyValuePair<string,string>>
             {
                 new("test", "1"),
                 new("test", "2"),
                 new("test", "3")
             });
-            Assert.Equal("https://localhost/mypath?test=1&test=2&test=3", httpRq.RequestUri.AbsoluteUri);
+            Assert.Equal("https://localhost/mypath?test=1&test=2&test=3", updatedUri.AbsoluteUri);
         }
 
         [Fact]
         public void AddQueryParameters_EscapeSpacesInValues()
         {
             var httpRq = new HttpRequestMessage(HttpMethod.Get, "https://localhost/mypath");
-            httpRq.AddQueryParameters(new List<KeyValuePair<string,string>>
+            var updatedUri = httpRq.RequestUri.AddQueryParameters(new List<KeyValuePair<string,string>>
             {
                 new("name1", "John Doe"),
                 new("name2", "Jane Doe")
             });
-            Assert.Equal("https://localhost/mypath?name1=John%20Doe&name2=Jane%20Doe", httpRq.RequestUri.AbsoluteUri);
+            Assert.Equal("https://localhost/mypath?name1=John%20Doe&name2=Jane%20Doe", updatedUri.AbsoluteUri);
         }
     }
 }
