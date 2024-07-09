4ca40eb34bb8d76665128bdd75ce408a35a95a57
Whit Waldo <whit.waldo@innovian.net>
2024-07-09 07:10:30 -0500

    Updated to support DaprJobClientOptions which specify the App ID and namespace the job is being requested by
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.Jobs/DaprJobClientOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace Dapr.Jobs;
+
+/// <summary>
+/// Options used to configure the Dapr job client.
+/// </summary>
+public class DaprJobClientOptions
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="DaprJobClientOptions"/>.
+    /// </summary>
+    /// <param name="appId">The ID of the app .</param>
+    /// <param name="appNamespace">The namespace of the app.</param>
+    public DaprJobClientOptions(string appId, string appNamespace)
+    {
+        AppId = appId;
+        Namespace = appNamespace;
+    }
+
+    /// <summary>
+    /// The App ID of the requester.
+    /// </summary>
+    public string AppId { get; init; }
+
+    /// <summary>
+    /// The namespace of the requester.
+    /// </summary>
+    public string Namespace { get; init; }
+}
