06efd3ab11935e7be4779435bc93033f58513a31
Manuel Menegazzo <mmenegazzo@battistellacompany.it>
2024-07-09 15:06:13 +0200

    Emitted DAPR001 Diagnostic warning
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

---
+++ src/Dapr.Actors.Generators/Diagnostics/CancellationTokensMustBeTheLastArgument.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Dapr.Actors.Generators.Diagnostics
+{
+    internal static class CancellationTokensMustBeTheLastArgument
+    {
+        public const string DiagnosticId = "DAPR0001";
+        public const string Title = "Invalid method signature";
+        public const string MessageFormat = "Cancellation tokens must be the last argument";
+        public const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        internal static Diagnostic CreateDiagnostic(ISymbol symbol, string reason) => Diagnostic.Create(
+            Rule,
+            symbol.Locations.First(),
+            symbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
+            reason);
+    }
+}
---
+++ src/Dapr.Actors.Generators/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Dapr.Actors.Generators.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Returns the index of the first item in the sequence that satisfies the predicate. If no item satisfies the predicate, -1 is returned.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns>Return the zero-based index of the first occurrence of an element that satisfies the condition, if found; otherwise, -1.</returns>
+        public static int IndexOf<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            int index = 0;
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return index;
+                }
+
+                index++;
+            }
+
+            return -1;
+        }
+    }
+}
