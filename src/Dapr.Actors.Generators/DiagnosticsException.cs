e8bd0dbe22d714f33bda1b23d67ca4f5a48775e6
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2024-07-10 21:59:00 +0200

    Moved DiagnoticException in a dedicate cs file
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

---
+++ src/Dapr.Actors.Generators/DiagnosticsException.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Dapr.Actors.Generators
+{
+    internal sealed class DiagnosticsException : Exception
+    {
+        public DiagnosticsException(IEnumerable<Diagnostic> diagnostics)
+            : base(string.Join("\n", diagnostics.Select(d => d.ToString())))
+        {
+            this.Diagnostics = diagnostics.ToArray();
+        }
+
+        public IEnumerable<Diagnostic> Diagnostics { get; }
+    }
+}
