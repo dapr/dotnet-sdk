7f1d241b328922256c3c1a33913d6907445397a8
Phillip Hoff <phillip@orst.edu>
2024-07-10 16:40:43 -0700

    Sketch addition of new pub/sub client.

---
+++ examples/Client/PublishSubscribe/StreamingSubscribeExample/README.md
@@ -0,0 +1,26 @@
+# Dapr .NET SDK Streaming Pub/Sub Example
+
+## Prerequisites
+
+- [.NET 6+](https://dotnet.microsoft.com/download) installed
+- [Dapr CLI](https://docs.dapr.io/getting-started/install-dapr-cli/)
+- [Initialized Dapr environment](https://docs.dapr.io/getting-started/install-dapr-selfhost/)
+- [Dapr .NET SDK](https://docs.dapr.io/developing-applications/sdks/dotnet/)
+
+## Running the example
+
+To run the sample locally run this command in the DaprClient directory:
+
+```sh
+dapr run --app-id test --dapr-http-port 3500 -- dotnet run
+```
+
+## Publishing Pub/Sub Events
+
+```bash
+curl -X POST http://localhost:3500/v1.0/publish/pubsub/topicA \
+  -H "Content-Type: application/json" \
+ -d '{
+       "hello": "world"
+     }'
+```
---
+++ src/Dapr.Messaging/PublishSubscribe/TopicRequest.cs
@@ -0,0 +1,65 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2024 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+namespace Dapr.Messaging.PublishSubscribe;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed record TopicRequest
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Id { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Source { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Type { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string SpecVersion { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string DataContentType { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Topic { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string PubSubName { get; init; } = default!;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Path { get; init; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public object? Extensions { get; init; } // TODO: Determine what this should look like.
+}
