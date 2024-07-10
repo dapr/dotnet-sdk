28db658df2be51b77d2b97f008c70e26a7b0b7ca
Manuel Menegazzo <mmenegazzo@battistellacompany.it>
2024-07-10 16:24:04 +0200

    Added nullable directive
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

---
+++ src/Dapr.Actors.Generators/Helpers/ExceptionHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Dapr.Actors.Generators.Helpers
+{
+    /// <summary>
+    /// Syntax factory helpers for generating exception syntax.
+    /// </summary>
+    public static partial class SyntaxFactoryHelpers
+    {
+        /// <summary>
+        /// Generates a syntax for ArgumentNullException syntax for the given parameter name.
+        /// </summary>
+        /// <param name="parameterName"></param>
+        /// <returns></returns>
+        public static ThrowExpressionSyntax ArgumentNullExceptionSyntax(string parameterName)
+        {
+            return SyntaxFactory.ThrowExpression(
+                SyntaxFactory.Token(SyntaxKind.ThrowKeyword),
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.ParseTypeName("System.ArgumentNullException"),
+                    SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new[]
+                    {
+                        SyntaxFactory.Argument(SyntaxFactory.IdentifierName(parameterName))
+                    }))
+                )
+            );
+        }
+    }
+}
