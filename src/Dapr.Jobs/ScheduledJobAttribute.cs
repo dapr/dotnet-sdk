e44dca0ae939b30da99d6038a84498128c05e192
Whit Waldo <whit.waldo@innovian.net>
2024-07-15 05:44:43 -0500

    Renamed to ScheduledJobAttribute to match the API name
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Jobs/ScheduledJobAttribute.cs
+++ src/Dapr.Jobs/ScheduledJobAttribute.cs
@@ -4,7 +4,7 @@
 /// Describes an endpoint as a subscriber for a job invocation.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-public sealed class JobAttribute : Attribute
+public sealed class ScheduledJobAttribute : Attribute
 {
     /// <summary>
     /// The name of the job.
@@ -12,10 +12,10 @@ public sealed class JobAttribute : Attribute
     public string JobName { get; init; }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="JobAttribute"/> class.
+    /// Initializes a new instance of the <see cref="ScheduledJobAttribute"/> class.
     /// </summary>
     /// <param name="jobName">The name of the job that invokes this method.</param>
-    public JobAttribute(string jobName)
+    public ScheduledJobAttribute(string jobName)
     {
         JobName = jobName;
     }
