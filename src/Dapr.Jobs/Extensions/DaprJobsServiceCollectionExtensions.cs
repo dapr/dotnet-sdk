c15fa6602b6146a3ba85508fa29851eb54a6eb75
Whit Waldo <whit.waldo@innovian.net>
2024-07-15 05:59:43 -0500

    Added another registration overload so the developer isn't forced to specify either of the action methods just to discard them.
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Jobs/Extensions/DaprJobsServiceCollectionExtensions.cs
+++ src/Dapr.Jobs/Extensions/DaprJobsServiceCollectionExtensions.cs
@@ -24,7 +24,26 @@ public static class DaprJobsServiceCollectionExtensions
     /// <summary>
     /// Adds Dapr Jobs client support to the service collection.
     /// </summary>
-    /// <param name="serviceCollection">The <see cref="IServiceCollection"/>.</param> <param name="configure">Optionally allows greater configuration of the <see cref="DaprJobsClient"/>.</param>
+    /// <param name="serviceCollection">The <see cref="IServiceCollection"/>.</param>
+    /// <returns></returns>
+    public static IServiceCollection AddDaprJobsClient(this IServiceCollection serviceCollection)
+    {
+        ArgumentNullException.ThrowIfNull(serviceCollection, nameof(serviceCollection));
+
+        serviceCollection.TryAddSingleton(_ =>
+        {
+            var builder = new DaprJobClientBuilder();
+            return builder.Build();
+        });
+
+        return serviceCollection;
+    }
+
+    /// <summary>
+    /// Adds Dapr Jobs client support to the service collection.
+    /// </summary>
+    /// <param name="serviceCollection">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configure">Optionally allows greater configuration of the <see cref="DaprJobsClient"/>.</param>
     public static IServiceCollection AddDaprJobsClient(this IServiceCollection serviceCollection, Action<DaprJobClientBuilder>? configure = null)
     {
         ArgumentNullException.ThrowIfNull(serviceCollection, nameof(serviceCollection));
