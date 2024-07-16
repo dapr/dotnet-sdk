60b1c888f9b85ecba95272725f39e2085c489bf1
Whit Waldo <whit.waldo@innovian.net>
2024-07-16 02:16:18 -0500

    Adding helper deserialization extension methods + tests
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ test/Dapr.Jobs.Test/ByteArrayDeserializationExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Dapr.Jobs.Extensions.Helpers.Deserialization;
+using Xunit;
+
+#nullable enable
+
+namespace Dapr.Jobs.Test;
+
+public class ByteArrayDeserializationExtensionsTest
+{
+    [Fact]
+    public void DeserializeToString_Deserialize()
+    {
+        const string originalStringValue = "This is a simple test!";
+        var serializedString = Encoding.UTF8.GetBytes(originalStringValue);
+
+        var deserializedString = serializedString.DeserializeToString();
+        Assert.Equal(originalStringValue, deserializedString);
+    }
+
+    [Fact]
+    public void DeserializeToJsonObject_Deserialize()
+    {
+        var originalType = new TestType { Name = "Test", Value = 5 };
+        var serialized = JsonSerializer.SerializeToUtf8Bytes(originalType);
+
+        var deserializedType = serialized.DeserializeFromJsonBytes<TestType>();
+        Assert.Equal(originalType, deserializedType);
+    }
+
+    [Fact]
+    public void DeserializeToJsonObject_DeserializeWithOptions()
+    {
+        const string json = "{\"value\": \"15\"}";
+        var jsonOptions = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, PropertyNameCaseInsensitive = true, NumberHandling = JsonNumberHandling.AllowReadingFromString};
+        var serializedBytes = Encoding.UTF8.GetBytes(json);
+
+        var deserializedType = serializedBytes.DeserializeFromJsonBytes<TestType>(jsonOptions);
+        Assert.NotNull(deserializedType);
+        Assert.Null(deserializedType.Name);
+        Assert.Equal(15, deserializedType.Value);
+    }
+
+    private sealed record TestType
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; init; } = null;
+
+        [JsonPropertyName("value")]
+        public int Value { get; init; }
+    }
+}
