efc31591cf04e17e6632ff614251cfd106b1dc7b
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2024-07-25 22:10:34 +0200

    Added test to IEnumerableExtensions
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

--- test/Dapr.Actors.Generators.Test/Extensions/IEnumerableExtensionsTests.cs
+++ test/Dapr.Actors.Generators.Test/Extensions/IEnumerableExtensionsTests.cs
@@ -17,5 +17,50 @@ namespace Dapr.Actors.Generators.Test.Extensions
             // Assert
             Assert.Throws<ArgumentNullException>(act);
         }
+
+        [Theory]
+        [InlineData(new int[] { }, 3, -1)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 6, -1)]
+        public void IndexOf_WhenItemDoesNotExist_ReturnsMinusOne(int[] source, int item, int expected)
+        {
+            // Arrange
+            Func<int, bool> predicate = (x) => x == item;
+
+            // Act
+            var index = source.IndexOf(predicate);
+
+            // Assert
+            Assert.Equal(expected, index);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 3, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 1, 0)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 5, 4)]
+        public void IndexOf_WhenItemExists_ReturnsIndexOfItem(int[] source, int item, int expected)
+        {
+            // Arrange
+            Func<int, bool> predicate = (x) => x == item;
+
+            // Act
+            var index = source.IndexOf(predicate);
+
+            // Assert
+            Assert.Equal(expected, index);
+        }
+
+        [Fact]
+        public void Concat_WhenItemIsNotNull_ReturnsConcatenatedSequence()
+        {
+            // Arrange
+            var source = new[] { "a", "b", "c" };
+            string item = "d";
+
+            // Act
+            var result = source.Concat(item);
+
+            // Assert
+            Assert.Equal(new[] { "a", "b", "c", "d" }, result);
+        }
     }
 }
