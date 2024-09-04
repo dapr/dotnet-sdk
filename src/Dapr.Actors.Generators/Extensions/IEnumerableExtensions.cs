89b40692a2d9f78d3b2cf29c466f1acbff592e3c
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2024-09-04 19:19:56 +0200

    Added some missing summaries
    Signed-off-by: Manuel Menegazzo <manuel.menegazzo@outlook.com>

--- src/Dapr.Actors.Generators/Extensions/IEnumerableExtensions.cs
+++ src/Dapr.Actors.Generators/Extensions/IEnumerableExtensions.cs
@@ -5,9 +5,9 @@
         /// <summary>
         /// Returns the index of the first item in the sequence that satisfies the predicate. If no item satisfies the predicate, -1 is returned.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="predicate"></param>
+        /// <typeparam name="T">Tipe of items.</typeparam>
+        /// <param name="source">Source enumerable where to search.</param>
+        /// <param name="predicate">Function performed to check whether an item satisfies the condition.</param>
         /// <returns>Return the zero-based index of the first occurrence of an element that satisfies the condition, if found; otherwise, -1.</returns>
         internal static int IndexOf<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
