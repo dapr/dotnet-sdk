849a82875798a5b5cb3a276e2cc02482a618297f
Whit Waldo <whit.waldo@innovian.net>
2024-09-04 08:42:38 -0500

    Added missing copyright notice
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Messaging/PublishSubscribe/DaprSubscriptionOptions.cs
+++ src/Dapr.Messaging/PublishSubscribe/DaprSubscriptionOptions.cs
@@ -1,4 +1,17 @@
-﻿namespace Dapr.Messaging.PublishSubscribe;
+﻿// ------------------------------------------------------------------------
+// Copyright 2024 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+namespace Dapr.Messaging.PublishSubscribe;
 
 /// <summary>
 /// Options used to configure the dynamic Dapr subscription.
@@ -12,7 +25,7 @@ public sealed record DaprSubscriptionOptions(MessageHandlingPolicy MessageHandli
     public IReadOnlyDictionary<string, string> Metadata { get; init; } = new Dictionary<string, string>();
     
     /// <summary>
-    /// The optional name of the dead-letter topic to send messages to.
+    /// The optional name of the dead-letter topic to send unprocessed messages to.
     /// </summary>
     public string? DeadLetterTopic { get; init; }
 }
