ac31e791903286a06f45ee9d332eedd2b37e85c8
Whit Waldo <whit.waldo@innovian.net>
2024-09-13 22:42:21 -0500

    Fixed string extensions functionality
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Jobs/Extensions/StringExtensions.cs
+++ src/Dapr.Jobs/Extensions/StringExtensions.cs
@@ -8,8 +8,5 @@ internal static class StringExtensions
     /// <param name="value">The string value to evaluate.</param>
     /// <param name="possibleValues">The possible values to look for a match within.</param>
     /// <returns></returns>
-    public static bool EndsWithAny(this string value, IReadOnlyList<string> possibleValues)
-    {
-        return possibleValues.Select(value.EndsWith).FirstOrDefault();
-    }
+    public static bool EndsWithAny(this string value, IReadOnlyList<string> possibleValues) => possibleValues.Any(value.EndsWith);
 }
