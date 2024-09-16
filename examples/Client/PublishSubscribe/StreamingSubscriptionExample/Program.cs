033a27a8346b0d10328acf1623970464eecc5966
Whit Waldo <whit.waldo@innovian.net>
2024-09-16 14:36:31 -0500

    Added example demonstrating the streaming subscription functionality
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ examples/Client/PublishSubscribe/StreamingSubscriptionExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapr.Messaging.PublishSubscribe;
+
+var daprMessagingClientBuilder = new DaprPublishSubscribeClientBuilder();
+var daprMessagingClient = daprMessagingClientBuilder.Build();
+
+//Processor for each of the messages returned from the subscription
+async Task<TopicResponseAction> HandleMessage(TopicMessage message, CancellationToken cancellationToken = default)
+{
+    try
+    {
+        //Do something with the message
+        return await Task.FromResult(TopicResponseAction.Success);
+    }
+    catch
+    {
+        return await Task.FromResult(TopicResponseAction.Retry);
+    }
+}
+
+//Create a dynamic streaming subscription
+var subscription = daprMessagingClient.Register("pubsub", "myTopic",
+    new DaprSubscriptionOptions(new MessageHandlingPolicy(TimeSpan.FromSeconds(15), TopicResponseAction.Retry)),
+    HandleMessage, CancellationToken.None);
+
+//Subscribe to messages on it with a timeout of 30 seconds
+var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+await subscription.SubscribeAsync(cancellationTokenSource.Token);
+
+await Task.Delay(TimeSpan.FromMinutes(1));
+
+//When you're done with the subscription, simply dispose of it
+await subscription.DisposeAsync();
