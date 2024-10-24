4b51d03b06380270490b9467d78715da7614e4e6
Rafael Camara <rafaelcamarac@gmail.com>
2024-10-24 19:04:43 +0200

    Small refactoring on dapr client and invocation handler. Add improvements on test structure.
    Signed-off-by: Rafael Camara <rafaelcamarac@gmail.com>

--- src/Dapr.Client/InvocationHandler.cs
+++ src/Dapr.Client/InvocationHandler.cs
@@ -13,148 +13,150 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 #nullable enable
 
-namespace Dapr.Client
+namespace Dapr.Client;
+
+/// <summary>
+/// <para>
+/// A <see cref="DelegatingHandler" /> implementation that rewrites URIs of outgoing requests
+/// to use the Dapr service invocation protocol. This handle allows code using <see cref="HttpClient" />
+/// to use the client as-if it were communciating with the destination application directly.
+/// </para>
+/// <para>
+/// The handler will read the <see cref="HttpRequestMessage.RequestUri" /> property, and 
+/// interpret the hostname as the destination <c>app-id</c>. The <see cref="HttpRequestMessage.RequestUri" /> 
+/// property will be replaced with a new URI with the authority section replaced by <see cref="DaprEndpoint" />
+/// and the path portion of the URI rewitten to follow the format of a Dapr service invocation request.
+/// </para>
+/// </summary>
+/// <remarks>
+/// This message handler does not distinguish between requests destined for Dapr service invocation and general
+/// HTTP traffic, and will attempt to forward all traffic to the Dapr endpoint. Do not attempt to set
+/// <see cref="DaprEndpoint" /> to a publicly routable URI, this will result in leaking of traffic and the Dapr
+/// security token.
+/// </remarks>
+public class InvocationHandler : DelegatingHandler
 {
+    private Uri parsedEndpoint;
+    private string? apiToken;
+    private readonly string[] supportedUriSchemes = { "http", "https" };
+
     /// <summary>
-    /// <para>
-    /// A <see cref="DelegatingHandler" /> implementation that rewrites URIs of outgoing requests
-    /// to use the Dapr service invocation protocol. This handle allows code using <see cref="HttpClient" />
-    /// to use the client as-if it were communciating with the destination application directly.
-    /// </para>
-    /// <para>
-    /// The handler will read the <see cref="HttpRequestMessage.RequestUri" /> property, and 
-    /// interpret the hostname as the destination <c>app-id</c>. The <see cref="HttpRequestMessage.RequestUri" /> 
-    /// property will be replaced with a new URI with the authority section replaced by <see cref="DaprEndpoint" />
-    /// and the path portion of the URI rewitten to follow the format of a Dapr service invocation request.
-    /// </para>
+    /// Initializes a new instance of <see cref="InvocationHandler" />.
     /// </summary>
-    /// <remarks>
-    /// This message handler does not distinguish between requests destined for Dapr service invocation and general
-    /// HTTP traffic, and will attempt to forward all traffic to the Dapr endpoint. Do not attempt to set
-    /// <see cref="DaprEndpoint" /> to a publicly routable URI, this will result in leaking of traffic and the Dapr
-    /// security token.
-    /// </remarks>
-    public class InvocationHandler : DelegatingHandler
+    public InvocationHandler()
     {
-        private Uri parsedEndpoint;
-        private string? apiToken;
+        this.parsedEndpoint = new Uri(DaprDefaults.GetDefaultHttpEndpoint(), UriKind.Absolute);
+        this.apiToken = DaprDefaults.GetDefaultDaprApiToken(null);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of <see cref="InvocationHandler" />.
-        /// </summary>
-        public InvocationHandler()
+    /// <summary>
+    /// Gets or the sets the URI of the Dapr HTTP endpoint used for service invocation.
+    /// </summary>
+    /// <returns>The URI of the Dapr HTTP endpoint used for service invocation.</returns>
+    public string DaprEndpoint
+    {
+        get => this.parsedEndpoint.OriginalString;
+            
+        set
         {
-            this.parsedEndpoint = new Uri(DaprDefaults.GetDefaultHttpEndpoint(), UriKind.Absolute);
-            this.apiToken = DaprDefaults.GetDefaultDaprApiToken(null);
-        }
+            ArgumentNullException.ThrowIfNull(value);
 
-        /// <summary>
-        /// Gets or the sets the URI of the Dapr HTTP endpoint used for service invocation.
-        /// </summary>
-        /// <returns>The URI of the Dapr HTTP endpoint used for service invocation.</returns>
-        public string DaprEndpoint
-        {
-            get
+            // This will throw a reasonable exception if the URI is invalid.
+            var uri = new Uri(value, UriKind.Absolute);
+                
+            if (!IsUriSchemeSupported(uri.Scheme))
             {
-                return this.parsedEndpoint.OriginalString;
-            }
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
-
-                // This will throw a reasonable exception if the URI is invalid.
-                var uri = new Uri(value, UriKind.Absolute);
-                if (uri.Scheme != "http" && uri.Scheme != "https")
-                {
-                    throw new ArgumentException("The URI scheme of the Dapr endpoint must be http or https.", "value");
-                }
-
-                this.parsedEndpoint = uri;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the default AppId used for service invocation
-        /// </summary>
-        /// <returns>The AppId used for service invocation</returns>
-        public string? DefaultAppId { get; set; }
-
-        // Internal for testing
-        internal string? DaprApiToken
-        {
-            get => this.apiToken;
-            set => this.apiToken = value;
-        }
-
-        /// <inheritdoc />
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            var original = request.RequestUri;
-            if (!this.TryRewriteUri(request.RequestUri, out var rewritten))
-            {
-                throw new ArgumentException($"The request URI '{original}' is not a valid Dapr service invocation destination.", nameof(request));
+                throw new ArgumentException("The URI scheme of the Dapr endpoint must be http or https.", nameof(value));
             }
 
-            try
-            {
-                var apiTokenHeader = DaprClient.GetDaprApiTokenHeader(this.apiToken);
-                if (apiTokenHeader is not null)
-                {
-                    request.Headers.Add(apiTokenHeader.Value.Key, apiTokenHeader.Value.Value);
-                }
-                request.RequestUri = rewritten;
-
-                return await base.SendAsync(request, cancellationToken);
-            }
-            finally
-            {
-                request.RequestUri = original;
-                request.Headers.Remove("dapr-api-token");
-            }
-        }
-
-        // Internal for testing
-        internal bool TryRewriteUri(Uri? uri, [NotNullWhen(true)] out Uri? rewritten)
-        {
-            // For now the only invalid cases are when the request URI is missing or just silly.
-            // We may support additional cases for validation in the future (like an allow-list of App-Ids).
-            if (uri is null || !uri.IsAbsoluteUri || (uri.Scheme != "http" && uri.Scheme != "https"))
-            {
-                // do nothing
-                rewritten = null;
-                return false;
-            }
-
-            string host;
-
-            if (this.DefaultAppId is not null && uri.Host.Equals(this.DefaultAppId, StringComparison.InvariantCultureIgnoreCase))
-            {
-                host = this.DefaultAppId;
-            }
-            else
-            {
-                host = uri.Host;
-            }
-
-            var builder = new UriBuilder(uri)
-            {
-                Scheme = this.parsedEndpoint.Scheme,
-                Host = this.parsedEndpoint.Host,
-                Port = this.parsedEndpoint.Port,
-                Path = $"/v1.0/invoke/{host}/method" + uri.AbsolutePath,
-            };
-
-            rewritten = builder.Uri;
-            return true;
+            this.parsedEndpoint = uri;
         }
     }
+
+    /// <summary>
+    /// Gets or sets the default AppId used for service invocation
+    /// </summary>
+    /// <returns>The AppId used for service invocation</returns>
+    public string? DefaultAppId { get; set; }
+
+    // Internal for testing
+    internal string? DaprApiToken
+    {
+        get => this.apiToken;
+        set => this.apiToken = value;
+    }
+
+    /// <inheritdoc />
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var originalUri = request.RequestUri;
+            
+        if (!this.TryRewriteUri(request.RequestUri, out var rewrittenUri))
+        {
+            throw new ArgumentException($"The request URI '{originalUri}' is not a valid Dapr service invocation destination.", nameof(request));
+        }
+
+        try
+        {
+            var apiTokenHeader = DaprClient.GetDaprApiTokenHeader(this.apiToken);
+                
+            if (apiTokenHeader is not null)
+            {
+                request.Headers.Add(apiTokenHeader.Value.Key, apiTokenHeader.Value.Value);
+            }
+                
+            request.RequestUri = rewrittenUri;
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+        finally
+        {
+            request.RequestUri = originalUri;
+            request.Headers.Remove("dapr-api-token");
+        }
+    }
+
+    // Internal for testing
+    internal bool TryRewriteUri(Uri? uri, [NotNullWhen(true)] out Uri? rewrittenUri)
+    {
+        if (!IsUriValid(uri))
+        {
+            // do nothing
+            rewrittenUri = null;
+            return false;
+        }
+
+        string host = UriHostContainsValidDefaultAppId(uri!.Host) ? this.DefaultAppId! : uri!.Host;
+
+        var builder = new UriBuilder(uri)
+        {
+            Scheme = this.parsedEndpoint.Scheme,
+            Host = this.parsedEndpoint.Host,
+            Port = this.parsedEndpoint.Port,
+            Path = $"/v1.0/invoke/{host}/method" + uri.AbsolutePath
+        };
+
+        rewrittenUri = builder.Uri;
+        return true;
+    }
+        
+    private bool IsUriSchemeSupported(string uriScheme)
+        => supportedUriSchemes.Contains(uriScheme, StringComparer.InvariantCultureIgnoreCase);
+
+    // For now the only invalid cases are when the request URI is missing or just silly.
+    // We may support additional cases for validation in the future (like an allow-list of App-Ids).
+    private bool IsUriValid(Uri? uri)
+        => uri is not null && uri.IsAbsoluteUri && IsUriSchemeSupported(uri.Scheme);
+
+    private bool UriHostContainsValidDefaultAppId(string uriHost)
+        => this.DefaultAppId is not null &&
+           uriHost.Equals(this.DefaultAppId, StringComparison.InvariantCultureIgnoreCase);
+
 }
---
+++ test/Dapr.Client.Test/TestData/InvocationHandlerTestsData.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+
+namespace Dapr.Client.Test.TestData;
+
+public class TryRewriteUriWithNoAppIdRewritesUriToDaprInvokeTestData : TheoryData<string, string, string>
+{
+    public TryRewriteUriWithNoAppIdRewritesUriToDaprInvokeTestData()
+    {
+        Add(null, "http://bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("bank", "http://bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("Bank", "http://bank", "https://some.host:3499/v1.0/invoke/Bank/method/");
+        Add("invalid", "http://bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://Bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("Bank", "http://Bank", "https://some.host:3499/v1.0/invoke/Bank/method/");
+        Add("bank", "http://Bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("invalid", "http://Bank", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://bank:3939", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("bank", "http://bank:3939", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("invalid", "http://bank:3939", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://Bank:3939", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("Bank", "http://Bank:3939", "https://some.host:3499/v1.0/invoke/Bank/method/");
+        Add("invalid", "http://Bank:3939", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://app-id.with.dots", "https://some.host:3499/v1.0/invoke/app-id.with.dots/method/");
+        Add("app-id.with.dots", "http://app-id.with.dots",
+            "https://some.host:3499/v1.0/invoke/app-id.with.dots/method/");
+        Add("invalid", "http://app-id.with.dots", "https://some.host:3499/v1.0/invoke/app-id.with.dots/method/");
+        Add(null, "http://App-id.with.dots", "https://some.host:3499/v1.0/invoke/app-id.with.dots/method/");
+        Add("App-id.with.dots", "http://App-id.with.dots",
+            "https://some.host:3499/v1.0/invoke/App-id.with.dots/method/");
+        Add("invalid", "http://App-id.with.dots", "https://some.host:3499/v1.0/invoke/app-id.with.dots/method/");
+        Add(null, "http://bank:3939/", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("bank", "http://bank:3939/", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("invalid", "http://bank:3939/", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://Bank:3939/", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add("Bank", "http://Bank:3939/", "https://some.host:3499/v1.0/invoke/Bank/method/");
+        Add("invalid", "http://Bank:3939/", "https://some.host:3499/v1.0/invoke/bank/method/");
+        Add(null, "http://bank:3939/some/path", "https://some.host:3499/v1.0/invoke/bank/method/some/path");
+        Add("bank", "http://bank:3939/some/path", "https://some.host:3499/v1.0/invoke/bank/method/some/path");
+        Add("invalid", "http://bank:3939/some/path", "https://some.host:3499/v1.0/invoke/bank/method/some/path");
+        Add(null, "http://Bank:3939/some/path", "https://some.host:3499/v1.0/invoke/bank/method/some/path");
+        Add("Bank", "http://Bank:3939/some/path", "https://some.host:3499/v1.0/invoke/Bank/method/some/path");
+        Add("invalid", "http://Bank:3939/some/path", "https://some.host:3499/v1.0/invoke/bank/method/some/path");
+        Add(null, "http://bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/bank/method/some/path?q=test&p=another#fragment");
+        Add("bank", "http://bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/bank/method/some/path?q=test&p=another#fragment");
+        Add("invalid", "http://bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/bank/method/some/path?q=test&p=another#fragment");
+        Add(null, "http://Bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/bank/method/some/path?q=test&p=another#fragment");
+        Add("Bank", "http://Bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/Bank/method/some/path?q=test&p=another#fragment");
+        Add("invalid", "http://Bank:3939/some/path?q=test&p=another#fragment",
+            "https://some.host:3499/v1.0/invoke/bank/method/some/path?q=test&p=another#fragment");
+    }
+}
