38f9473f395cd142fe09c33576c3e6647f0d1d86
Whit Waldo <whit.waldo@innovian.net>
2024-10-30 15:01:18 -0500

    Added async workflow example + copyright headers
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ examples/Workflow/WorkflowAsyncApi/Program.cs
@@ -0,0 +1,48 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2021 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+using Dapr.Workflow;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkflowAsyncApi.Activities;
+using WorkflowAsyncApi.Models;
+using WorkflowAsyncApi.Workflows;
+
+var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+{
+    services.AddDaprWorkflow(options =>
+    {
+        options.RegisterWorkflow<DemoWorkflow>();
+        options.RegisterActivity<ProcessPaymentActivity>();
+        options.RegisterActivity<NotifyWarehouseActivity>();
+    });
+});
+
+var host = await builder.StartAsync();
+
+await using var scope = host.Services.CreateAsyncScope();
+var daprWorkflowClient = scope.ServiceProvider.GetRequiredService<DaprWorkflowClient>();
+
+var instanceId = $"demo-workflow-{Guid.NewGuid().ToString()[..8]}";
+var transaction = new Transaction(16.58m);
+await daprWorkflowClient.ScheduleNewWorkflowAsync(nameof(DemoWorkflow), instanceId, transaction);
+
+//Poll for status updates every second
+var status = await daprWorkflowClient.GetWorkflowStateAsync(instanceId);
+do
+{
+    Console.WriteLine($"Current status: {status.RuntimeStatus}, step: {status.ReadCustomStatusAs<string>()}");
+    status = await daprWorkflowClient.GetWorkflowStateAsync(instanceId);
+} while (!status.IsWorkflowCompleted);
+
+Console.WriteLine($"Workflow completed - {status.ReadCustomStatusAs<string>()}");
---
+++ examples/Workflow/WorkflowAsyncApi/Workflows/DemoWorkflow.cs
@@ -0,0 +1,51 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2021 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+using Dapr.Workflow;
+using WorkflowAsyncApi.Activities;
+using WorkflowAsyncApi.Models;
+
+namespace WorkflowAsyncApi.Workflows;
+
+internal sealed class DemoWorkflow : Workflow<Transaction, bool>
+{
+    /// <summary>
+    /// Override to implement workflow logic.
+    /// </summary>
+    /// <param name="context">The workflow context.</param>
+    /// <param name="input">The deserialized workflow input.</param>
+    /// <returns>The output of the workflow as a task.</returns>
+    public override async Task<bool> RunAsync(WorkflowContext context, Transaction input)
+    {
+        try
+        {
+            //Submit the transaction to the payment processor
+            context.SetCustomStatus("Processing payment...");
+            await context.CallActivityAsync(nameof(ProcessPaymentActivity), input);
+            
+
+            //Send the transaction details to the warehouse
+            context.SetCustomStatus("Contacting warehouse...");
+            await context.CallActivityAsync(nameof(NotifyWarehouseActivity), input);
+
+            context.SetCustomStatus("Success!");
+            return true;
+        }
+        catch
+        {
+            //If anything goes wrong, return false
+            context.SetCustomStatus("Something went wrong");
+            return false;
+        }
+    }
+}
