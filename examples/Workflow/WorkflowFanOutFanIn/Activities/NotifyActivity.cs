b4530551795e1c45deff6d4e3b191a07b4c51af8
Whit Waldo <whit.waldo@innovian.net>
2024-10-30 14:22:50 -0500

    Added workflow fan out/fan in example
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ examples/Workflow/WorkflowFanOutFanIn/Activities/NotifyActivity.cs
@@ -0,0 +1,18 @@
+﻿using Dapr.Workflow;
+
+namespace WorkflowFanOutFanIn.Activities;
+
+internal sealed class NotifyActivity : WorkflowActivity<string, object?>
+{
+    /// <summary>
+    /// Override to implement async (non-blocking) workflow activity logic.
+    /// </summary>
+    /// <param name="context">Provides access to additional context for the current activity execution.</param>
+    /// <param name="input">The deserialized activity input.</param>
+    /// <returns>The output of the activity as a task.</returns>
+    public override Task<object?> RunAsync(WorkflowActivityContext context, string input)
+    {
+        Console.WriteLine(input);
+        return Task.FromResult<object?>(null);
+    }
+}
---
+++ examples/Workflow/WorkflowFanOutFanIn/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dapr.Workflow;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkflowFanOutFanIn.Activities;
+using WorkflowFanOutFanIn.Workflows;
+
+var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+{
+    services.AddDaprWorkflow(options =>
+    {
+        options.RegisterWorkflow<DemoWorkflow>();
+        options.RegisterActivity<NotifyActivity>();
+    });
+});
+
+var host = builder.Build();
+await host.StartAsync();
+
+await using var scope = host.Services.CreateAsyncScope();
+var daprWorkflowClient = scope.ServiceProvider.GetRequiredService<DaprWorkflowClient>();
+
+var instanceId = $"workflow-demo-{Guid.NewGuid().ToString()[..8]}";
+await daprWorkflowClient.ScheduleNewWorkflowAsync(nameof(DemoWorkflow), instanceId, "test input");
+
+await daprWorkflowClient.WaitForWorkflowCompletionAsync(instanceId);
+var state = await daprWorkflowClient.GetWorkflowStateAsync(instanceId);
+Console.WriteLine($"Workflow state: {state.RuntimeStatus}");
