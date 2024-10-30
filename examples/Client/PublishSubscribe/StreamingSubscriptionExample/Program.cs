8febaa50db5447cedf4f661b657d6dd48162ebff
Whit Waldo <whit.waldo@innovian.net>
2024-10-30 06:27:39 -0500

    Updated example to use DI registration
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- examples/Client/PublishSubscribe/StreamingSubscriptionExample/Program.cs
+++ examples/Client/PublishSubscribe/StreamingSubscriptionExample/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text;
 using Dapr.Messaging.PublishSubscribe;
+using Dapr.Messaging.PublishSubscribe.Extensions;
 
-var daprMessagingClientBuilder = new DaprPublishSubscribeClientBuilder(null);
-var daprMessagingClient = daprMessagingClientBuilder.Build();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDaprPubSubClient();
+var app = builder.Build();
 
 //Process each message returned from the subscription
 Task<TopicResponseAction> HandleMessageAsync(TopicMessage message, CancellationToken cancellationToken = default)
@@ -19,9 +21,11 @@ Task<TopicResponseAction> HandleMessageAsync(TopicMessage message, CancellationT
     }
 }
 
-//Create a dynamic streaming subscription and subscribe with a timeout of 20 seconds and 10 seconds for message handling
-var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(20));
-var subscription = await daprMessagingClient.SubscribeAsync("pubsub", "myTopic",
+var messagingClient = app.Services.GetRequiredService<DaprPublishSubscribeClient>();
+
+//Create a dynamic streaming subscription and subscribe with a timeout of 30 seconds and 10 seconds for message handling
+var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+var subscription = await messagingClient.SubscribeAsync("pubsub", "myTopic",
     new DaprSubscriptionOptions(new MessageHandlingPolicy(TimeSpan.FromSeconds(10), TopicResponseAction.Retry)),
     HandleMessageAsync, cancellationTokenSource.Token);
 
