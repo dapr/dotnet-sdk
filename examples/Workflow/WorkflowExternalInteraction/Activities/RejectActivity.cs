650c34867d29b0a9c25a2daa488c92f4fcca3dc2
Whit Waldo <whit.waldo@innovian.net>
2024-10-30 13:47:47 -0500

    Added workflow example demonstrating external interaction
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ examples/Workflow/WorkflowExternalInteraction/Activities/RejectActivity.cs
@@ -0,0 +1,20 @@
+﻿using Dapr.Workflow;
+
+namespace WorkflowExternalInteraction.Activities;
+
+internal sealed class RejectActivity : WorkflowActivity<string, bool>
+{
+    /// <summary>
+    /// Override to implement async (non-blocking) workflow activity logic.
+    /// </summary>
+    /// <param name="context">Provides access to additional context for the current activity execution.</param>
+    /// <param name="input">The deserialized activity input.</param>
+    /// <returns>The output of the activity as a task.</returns>
+    public override async Task<bool> RunAsync(WorkflowActivityContext context, string input)
+    {
+        Console.WriteLine($"Workflow {input} is rejected");
+        Console.WriteLine("Running Reject activity...");
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        return true;
+    }
+}
---
+++ examples/Workflow/WorkflowExternalInteraction/Program.cs
@@ -0,0 +1,63 @@
+﻿using Dapr.Workflow;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkflowExternalInteraction.Activities;
+using WorkflowExternalInteraction.Workflows;
+
+var builder = Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+{
+    services.AddDaprWorkflow(options =>
+    {
+        options.RegisterWorkflow<DemoWorkflow>();
+        options.RegisterActivity<ApproveActivity>();
+        options.RegisterActivity<RejectActivity>();
+    });
+});
+
+using var host = builder.Build();
+await host.StartAsync();
+
+await using var scope = host.Services.CreateAsyncScope();
+var daprWorkflowClient = scope.ServiceProvider.GetRequiredService<DaprWorkflowClient>();
+
+var instanceId = $"demo-workflow-{Guid.NewGuid().ToString()[..8]}";
+
+await daprWorkflowClient.ScheduleNewWorkflowAsync(nameof(DemoWorkflow), instanceId, instanceId);
+
+
+bool enterPressed = false;
+Console.WriteLine("Press [ENTER] within the next 10 seconds to approve this workflow");
+using (var cts = new CancellationTokenSource())
+{
+    var inputTask = Task.Run(() =>
+    {
+        if (Console.ReadKey().Key == ConsoleKey.Enter)
+        {
+            Console.WriteLine("Approved");
+            enterPressed = true;
+            cts.Cancel(); //Cancel the delay task if Enter is pressed
+        }
+    });
+
+    try
+    {
+        await Task.Delay(TimeSpan.FromSeconds(10), cts.Token);
+    }
+    catch (TaskCanceledException)
+    {
+        // Task was cancelled because Enter was pressed
+    }
+}
+
+if (enterPressed)
+{
+    await daprWorkflowClient.RaiseEventAsync(instanceId, "Approval", true);
+}
+else
+{
+    Console.WriteLine("Rejected");
+}
+
+await daprWorkflowClient.WaitForWorkflowCompletionAsync(instanceId);
+var state = await daprWorkflowClient.GetWorkflowStateAsync(instanceId);
+Console.WriteLine($"Workflow state: {state.RuntimeStatus}");
