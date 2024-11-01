f51571dd1649a1470c45089ec259ac47f19756bc
Whit Waldo <whit.waldo@innovian.net>
2024-11-01 12:22:48 -0500

    Removed duplicate using statement breaking build
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Common/DaprGenericClientBuilder.cs
+++ src/Dapr.Common/DaprGenericClientBuilder.cs
@@ -12,7 +12,6 @@
 // ------------------------------------------------------------------------
 
 using System.Text.Json;
-using System.Text.Json;
 using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
 
