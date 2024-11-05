91ee78aff45621f8801485837875f6e916ffcf4d
Whit Waldo <whit.waldo@innovian.net>
2024-11-05 11:57:21 -0600

    Add .NET client for pub/sub support - streaming subscriptions (#1381)
    * Building out Dapr.Messaging and test project for streaming pubsub subscriptions
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added copyright notices
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Minor stylistic updates
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added generic client builder to support publish/subscribe client builder
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Tweaked XML comment
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added several unit tests for the generic client builder
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Updated to include latest review changes:
    - Added lock so that while we guarantee the method is called only once, it should be thread-safe now
    - Marked PublishSubscribeReceiver as internal so its members aren't part of the public API
    - Updated TopicMessage to use IReadOnlyDictionary
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Switched to interlock exchange instead of lock to slightly simplify code
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added sample project
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Minor changes to unit test
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Deleted protos folder
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Using lowercase protos dir name
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added registration extension methods
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Updated example to use DI registration
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Added default cancellation token
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Passing stream into method instead of creating it twice
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    ---------
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.Messaging/PublishSubscribe/DaprSubscriptionOptions.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2024 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+namespace Dapr.Messaging.PublishSubscribe;
+
+/// <summary>
+/// Options used to configure the dynamic Dapr subscription.
+/// </summary>
+/// <param name="MessageHandlingPolicy">Describes the policy to take on messages that have not been acknowledged within the timeout period.</param>
+public sealed record DaprSubscriptionOptions(MessageHandlingPolicy MessageHandlingPolicy)
+{
+    /// <summary>
+    /// Subscription metadata.
+    /// </summary>
+    public IReadOnlyDictionary<string, string> Metadata { get; init; } = new Dictionary<string, string>();
+    
+    /// <summary>
+    /// The optional name of the dead-letter topic to send unprocessed messages to.
+    /// </summary>
+    public string? DeadLetterTopic { get; init; }
+
+    /// <summary>
+    /// If populated, this reflects the maximum number of messages that can be queued for processing on the replica. By default,
+    /// no maximum boundary is enforced.
+    /// </summary>
+    public int? MaximumQueuedMessages { get; init; }
+
+    /// <summary>
+    /// The maximum amount of time to take to dispose of acknowledgement messages after the cancellation token has
+    /// been signaled.
+    /// </summary>
+    public TimeSpan MaximumCleanupTimeout { get; init; } = TimeSpan.FromSeconds(30);
+}
+
