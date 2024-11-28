0ccda6b8fcdf05bdfd37b6cdbe3c6feb65578cf6
Whit Waldo <whit.waldo@innovian.net>
2024-11-28 06:11:13 -0600

    Added more unit tests
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ test/Dapr.Messaging.Test/Extensions/PublishSubscribeServiceCollectionExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using Dapr.Messaging.PublishSubscribe;
+using Dapr.Messaging.PublishSubscribe.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Dapr.Messaging.Test.Extensions;
+
+public class PublishSubscribeServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddDaprPubSubClient_RegistersServicesCorrectly()
+    {
+        var services = new ServiceCollection();
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        services.AddSingleton(httpClientFactoryMock.Object);
+
+        services.AddDaprPubSubClient();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var daprClient = serviceProvider.GetService<DaprPublishSubscribeClient>();
+        Assert.NotNull(daprClient);
+    }
+
+    [Fact]
+    public void AddDaprPubSubClient_CallsConfigureAction()
+    {
+        var services = new ServiceCollection();
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        services.AddSingleton(httpClientFactoryMock.Object);
+
+        var configureCalled = false;
+
+        services.AddDaprPubSubClient(Configure);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var daprClient = serviceProvider.GetService<DaprPublishSubscribeClient>();
+        Assert.NotNull(daprClient);
+        Assert.True(configureCalled);
+        return;
+
+        void Configure(IServiceProvider sp, DaprPublishSubscribeClientBuilder builder)
+        {
+            configureCalled = true;
+        }
+    }   
+}
