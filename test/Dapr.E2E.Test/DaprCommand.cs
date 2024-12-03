7965359d2cbb8f270f086acd6583fd7bf4f35644
paule96 <paul-jeschke@outlook.com>
2024-12-03 21:26:00 +0100

    Bug/476 multiple methods per interface with JSON serialization doesn´t work (#1343)
    * update devcontainer
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * update test setup
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * Now the json serialization should work with multiple methods in an interface
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * fixed devcontainer to run actors
    
    Now the devcontainer uses docker in docker, so you can reach the dapr setup after you did run dapr init. This will then only affect the dev container, without compromising the host of the devcontainer
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * fix bugs with the current implementation
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * add a test that checks excatly the behavior
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * fix devcontainer post creatd command
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * change the default to dotnet 8.0
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * I don't know what is different but we commit.
    
    Maybe it resolves the need of chmod for it 🤷‍♀️
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * make it easier to see why the application of an E2E test couldn't start
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * make the exception in E2E more percise
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    * fix exception message
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    
    ---------
    
    Signed-off-by: paule96 <paul-jeschke@outlook.com>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>
    Co-authored-by: Whit Waldo <whit.waldo@innovian.net>
    Signed-off-by: Divya Perumal <divzi.perumal@gmail.com>

--- test/Dapr.E2E.Test/DaprCommand.cs
+++ test/Dapr.E2E.Test/DaprCommand.cs
@@ -16,6 +16,7 @@ namespace Dapr.E2E.Test
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Drawing;
     using System.Linq;
     using System.Threading;
     using Xunit.Abstractions;
@@ -23,6 +24,7 @@ namespace Dapr.E2E.Test
     public class DaprCommand
     {
         private readonly ITestOutputHelper output;
+        private readonly CircularBuffer<string> logBuffer = new CircularBuffer<string>(1000);
 
         public DaprCommand(ITestOutputHelper output)
         {
@@ -66,7 +68,12 @@ namespace Dapr.E2E.Test
             var done = outputReceived.WaitOne(this.Timeout);
             if (!done)
             {
-                throw new Exception($"Command: \"{this.Command}\" timed out while waiting for output: \"{this.OutputToMatch}\"");
+                var ex = new Exception($"Command: \"{this.Command}\" timed out while waiting for output: \"{this.OutputToMatch}\"{System.Environment.NewLine}" + 
+                "This could also mean the E2E app had a startup error. For more details see the Data property of this exception.");
+                // we add here the log buffer of the last 1000 lines, of the application log
+                // to make it easier to debug failing tests
+                ex.Data.Add("log", this.logBuffer.ToArray());
+                throw ex;
             }
         }
 
@@ -79,8 +86,7 @@ namespace Dapr.E2E.Test
 
             try
             {
-                // see: https://github.com/xunit/xunit/issues/2146
-                this.output.WriteLine(e.Data.TrimEnd(Environment.NewLine.ToCharArray()));
+                WriteLine(e.Data);
             }
             catch (InvalidOperationException)
             {
@@ -101,12 +107,81 @@ namespace Dapr.E2E.Test
 
             try
             {
-                // see: https://github.com/xunit/xunit/issues/2146
-                this.output.WriteLine(e.Data.TrimEnd(Environment.NewLine.ToCharArray()));
+                WriteLine(e.Data);
             }
             catch (InvalidOperationException)
             {
             }
+        }
+
+        private void WriteLine(string message)
+        {
+            // see: https://github.com/xunit/xunit/issues/2146
+            var formattedMessage = message.TrimEnd(Environment.NewLine.ToCharArray());
+            this.output.WriteLine(formattedMessage);
+            this.logBuffer.Add(formattedMessage);
+        }
+    }
+
+    /// <summary>
+    /// A circular buffer that can be used to store a fixed number of items.
+    /// When the buffer is full, the oldest item is overwritten.
+    /// The buffer can be read in the same order as the items were added.
+    /// More information can be found <see href="https://en.wikipedia.org/wiki/Circular_buffer">here</see>.
+    /// </summary>
+    /// <remarks>
+    /// The buffer gets initialized by the call to the constructor and will allocate,
+    /// the memory for the buffer. The buffer is not resizable.
+    /// That means be carefull with <see cref="size"/>, because it can cause an <see cref="OutOfMemoryException"/>.
+    /// </remarks>
+    /// <typeparam name="T">The type of what the cicular buffer is off.</typeparam>
+    internal class CircularBuffer<T>{
+        private readonly int size;
+        private readonly T[] buffer;
+        private int readPosition = 0;
+        private int writePosition = 0;
+        /// <summary>
+        /// Initialize the buffer with the buffer size of <paramref name="size"/>.
+        /// </summary>
+        /// <param name="size">
+        /// The size the buffer will have
+        /// </param>
+        public CircularBuffer(int size)
+        {
+            this.size = size;
+            buffer = new T[size];
+        }
+        /// <summary>
+        /// Adds an item and move the write position to the next value
+        /// </summary>
+        /// <param name="item">The item that should be written.</param>
+        public void Add(T item)
+        {
+            buffer[writePosition] = item;
+            writePosition = (writePosition + 1) % size;
+        }
+        /// <summary>
+        /// Reads on value and move the position to the next value
+        /// </summary>
+        /// <returns></returns>
+        public T Read(){
+            var value = buffer[readPosition];
+            readPosition = (readPosition + 1) % size;
+            return value;
+        }
+        /// <summary>
+        /// Read the full buffer. 
+        /// While the buffer is read, the read position is moved to the next value
+        /// </summary>
+        /// <returns></returns>
+        public T[] ToArray()
+        {
+            var result = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = Read();
+            }
+            return result;
         }
     }
 }
