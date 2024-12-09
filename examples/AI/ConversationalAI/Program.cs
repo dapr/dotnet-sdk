05c80555d19d56f86fa685d5678f62601d5709ca
Whit Waldo <whit.waldo@innovian.net>
2024-12-09 10:20:34 -0600

    Removed impossible path since the role cannot be nullable
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- examples/AI/ConversationalAI/Program.cs
+++ examples/AI/ConversationalAI/Program.cs
@@ -8,8 +8,13 @@ builder.Services.AddDaprAiConversation();
 var app = builder.Build();
 
 var conversationClient = app.Services.GetRequiredService<DaprConversationClient>();
-var response = await conversationClient.ConverseAsync("replace-with-component-name",
-    new List<DaprConversationInput> { new DaprConversationInput("Hello - anyone out there?") });
+var response = await conversationClient.ConverseAsync("conversation",
+    new List<DaprConversationInput>
+    {
+        new DaprConversationInput(
+            "Please write a witty haiku about the Dapr distributed programming framework at dapr.io",
+            DaprConversationRole.Generic)
+    });
 
 Console.WriteLine("Received the following from the LLM:");
 foreach (var resp in response.Outputs)
