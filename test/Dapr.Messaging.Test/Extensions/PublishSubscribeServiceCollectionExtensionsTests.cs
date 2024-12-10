7174ac7c57e4637fd31a32cfc9cc55f3da770117
Whit Waldo <whit.waldo@innovian.net>
2024-12-10 09:23:50 -0600

    Added additional unit test for consistency between client projects + copyright header
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- test/Dapr.Messaging.Test/Extensions/PublishSubscribeServiceCollectionExtensionsTests.cs
+++ test/Dapr.Messaging.Test/Extensions/PublishSubscribeServiceCollectionExtensionsTests.cs
@@ -1,11 +1,50 @@
-﻿using Dapr.Messaging.PublishSubscribe;
+﻿// ------------------------------------------------------------------------
+// Copyright 2024 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+using Dapr.Messaging.PublishSubscribe;
 using Dapr.Messaging.PublishSubscribe.Extensions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dapr.Messaging.Test.Extensions;
 
 public sealed class PublishSubscribeServiceCollectionExtensionsTests
 {
+    [Fact]
+    public void AddDaprMessagingClient_FromIConfiguration()
+    {
+        const string apiToken = "abc123";
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                {"DAPR_API_TOKEN", apiToken }
+            })
+            .Build();
+
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IConfiguration>(configuration);
+
+        services.AddDaprPubSubClient();
+
+        var app = services.BuildServiceProvider();
+
+        var pubSubClient = app.GetRequiredService<DaprPublishSubscribeClient>() as DaprPublishSubscribeGrpcClient;
+
+        Assert.NotNull(pubSubClient!);
+        Assert.Equal(apiToken, pubSubClient.DaprApiToken);
+    }
+    
     [Fact]
     public void AddDaprPubSubClient_RegistersIHttpClientFactory()
     {
