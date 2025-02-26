1d2cfdc93f7a60fabbc9870c5e0ab85bf1e4ef51
Whit Waldo <whit.waldo@innovian.net>
2025-02-26 06:16:34 -0600

    Added more checks to determine if a reminder was returned or not having discovered that the runtime returns a 200 even if there's no reminder registered.
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Actors/Runtime/ReminderInfo.cs
+++ src/Dapr.Actors/Runtime/ReminderInfo.cs
@@ -11,99 +11,109 @@
 // limitations under the License.
 // ------------------------------------------------------------------------
 
-namespace Dapr.Actors.Runtime
+#nullable enable
+namespace Dapr.Actors.Runtime;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// represents the wire format used by Dapr to store reminder info with the runtime
+internal class ReminderInfo
 {
-    using System;
-    using System.IO;
-    using System.Text;
-    using System.Text.Json;
-    using System.Threading.Tasks;
-
-    // represents the wire format used by Dapr to store reminder info with the runtime
-    internal class ReminderInfo
+    public ReminderInfo(
+        byte[] data,
+        TimeSpan dueTime,
+        TimeSpan period,
+        int? repetitions = null,
+        TimeSpan? ttl = null)
     {
-        public ReminderInfo(
-            byte[] data,
-            TimeSpan dueTime,
-            TimeSpan period,
-            int? repetitions = null,
-            TimeSpan? ttl = null)
-        {
-            this.Data = data;
-            this.DueTime = dueTime;
-            this.Period = period;
-            this.Ttl = ttl;
-            this.Repetitions = repetitions;
-        }
+        this.Data = data;
+        this.DueTime = dueTime;
+        this.Period = period;
+        this.Ttl = ttl;
+        this.Repetitions = repetitions;
+    }
 
-        public TimeSpan DueTime { get; private set; }
+    public TimeSpan DueTime { get; private set; }
 
-        public TimeSpan Period { get; private set; }
+    public TimeSpan Period { get; private set; }
 
-        public byte[] Data { get; private set; }
+    public byte[] Data { get; private set; }
 
-        public TimeSpan? Ttl { get; private set; }
+    public TimeSpan? Ttl { get; private set; }
         
-        public int? Repetitions { get; private set; }
+    public int? Repetitions { get; private set; }
 
-        internal static async Task<ReminderInfo> DeserializeAsync(Stream stream)
+    internal static async Task<ReminderInfo?> DeserializeAsync(Stream stream)
+    {
+        var json = await JsonSerializer.DeserializeAsync<JsonElement>(stream);
+        if(json.ValueKind == JsonValueKind.Null)
         {
-            var json = await JsonSerializer.DeserializeAsync<JsonElement>(stream);
-            if(json.ValueKind == JsonValueKind.Null)
-            {
-                return null;
-            }
-
-            var dueTime = default(TimeSpan);
-            var period = default(TimeSpan);
-            var data = default(byte[]);
-            int? repetition = null;
-            TimeSpan? ttl = null;
-            if (json.TryGetProperty("dueTime", out var dueTimeProperty))
-            {
-                var dueTimeString = dueTimeProperty.GetString();
-                dueTime = ConverterUtils.ConvertTimeSpanFromDaprFormat(dueTimeString);
-            }
-
-            if (json.TryGetProperty("period", out var periodProperty))
-            {
-                var periodString = periodProperty.GetString();
-                (period, repetition) = ConverterUtils.ConvertTimeSpanValueFromISO8601Format(periodString);
-            }
-
-            if (json.TryGetProperty("data", out var dataProperty) && dataProperty.ValueKind != JsonValueKind.Null)
-            {
-                data = dataProperty.GetBytesFromBase64();
-            }
-
-            if (json.TryGetProperty("ttl", out var ttlProperty))
-            {
-                var ttlString = ttlProperty.GetString();
-                ttl = ConverterUtils.ConvertTimeSpanFromDaprFormat(ttlString);
-            }
-
-            return new ReminderInfo(data, dueTime, period, repetition, ttl);
+            return null;
         }
 
-        internal async ValueTask<string> SerializeAsync()
+        var setAnyProperties = false; //Used to determine if anything was actually deserialized
+        var dueTime = TimeSpan.Zero;
+        var period = TimeSpan.Zero;
+        var data = Array.Empty<byte>();
+        int? repetition = null;
+        TimeSpan? ttl = null;
+        if (json.TryGetProperty("dueTime", out var dueTimeProperty))
         {
-            using var stream = new MemoryStream();
-            using Utf8JsonWriter writer = new Utf8JsonWriter(stream);
-
-            writer.WriteStartObject();
-            writer.WriteString("dueTime", ConverterUtils.ConvertTimeSpanValueInDaprFormat(this.DueTime));
-            writer.WriteString("period", ConverterUtils.ConvertTimeSpanValueInISO8601Format(
-                this.Period, this.Repetitions));
-            writer.WriteBase64String("data", this.Data);
-
-            if (Ttl != null)
-            {
-                writer.WriteString("ttl", ConverterUtils.ConvertTimeSpanValueInDaprFormat(Ttl));
-            }
-
-            writer.WriteEndObject();
-            await writer.FlushAsync();
-            return Encoding.UTF8.GetString(stream.ToArray());
+            setAnyProperties = true;
+            var dueTimeString = dueTimeProperty.GetString();
+            dueTime = ConverterUtils.ConvertTimeSpanFromDaprFormat(dueTimeString);
         }
+
+        if (json.TryGetProperty("period", out var periodProperty))
+        {
+            setAnyProperties = true;
+            var periodString = periodProperty.GetString();
+            (period, repetition) = ConverterUtils.ConvertTimeSpanValueFromISO8601Format(periodString);
+        }
+
+        if (json.TryGetProperty("data", out var dataProperty) && dataProperty.ValueKind != JsonValueKind.Null)
+        {
+            setAnyProperties = true;
+            data = dataProperty.GetBytesFromBase64();
+        }
+
+        if (json.TryGetProperty("ttl", out var ttlProperty))
+        {
+            setAnyProperties = true;
+            var ttlString = ttlProperty.GetString();
+            ttl = ConverterUtils.ConvertTimeSpanFromDaprFormat(ttlString);
+        }
+
+        if (!setAnyProperties)
+        {
+            return null; //No properties were ever deserialized, so return null instead of default values
+        }
+
+        return new ReminderInfo(data, dueTime, period, repetition, ttl);
+    }
+
+    internal async ValueTask<string> SerializeAsync()
+    {
+        using var stream = new MemoryStream();
+        await using Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+
+        writer.WriteStartObject();
+        writer.WriteString("dueTime", ConverterUtils.ConvertTimeSpanValueInDaprFormat(this.DueTime));
+        writer.WriteString("period", ConverterUtils.ConvertTimeSpanValueInISO8601Format(
+            this.Period, this.Repetitions));
+        writer.WriteBase64String("data", this.Data);
+
+        if (Ttl != null)
+        {
+            writer.WriteString("ttl", ConverterUtils.ConvertTimeSpanValueInDaprFormat(Ttl));
+        }
+
+        writer.WriteEndObject();
+        await writer.FlushAsync();
+        return Encoding.UTF8.GetString(stream.ToArray());
     }
 }
