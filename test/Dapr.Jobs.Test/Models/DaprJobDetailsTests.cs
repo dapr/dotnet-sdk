b7d6bb44c5ed8a04a1367e8732414f9fcf15f459
Whit Waldo <whit.waldo@innovian.net>
2025-03-21 15:19:54 -0500

    Refactored slightly to make the deserialization for GetJobAsync testable. Added unit test to validate reported issue.

---
+++ test/Dapr.Jobs.Test/Models/DaprJobDetailsTests.cs
@@ -0,0 +1,35 @@
+﻿// ------------------------------------------------------------------------
+//  Copyright 2025 The Dapr Authors
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ------------------------------------------------------------------------
+
+using Dapr.Client.Autogen.Grpc.v1;
+using Dapr.Jobs.Models;
+
+namespace Dapr.Jobs.Test.Models;
+
+public class DaprJobDetailsTests
+{
+    [Fact]
+    public void ShouldDeserialize_EveryExpression()
+    {
+        const string scheduleText = "@every 1m";
+        var response = new GetJobResponse { Job = new Job { Name = "test", Schedule = scheduleText } };
+        var schedule = DaprJobSchedule.FromExpression(scheduleText);
+        
+        var jobDetails = DaprJobsGrpcClient.DeserializeJobResponse(response);
+        Assert.Null(jobDetails.Payload);
+        Assert.Equal(0, jobDetails.RepeatCount);
+        Assert.Null(jobDetails.Ttl);
+        Assert.Null(jobDetails.DueTime);
+        Assert.Equal(jobDetails.Schedule.ExpressionValue, schedule.ExpressionValue);
+    }
+}
