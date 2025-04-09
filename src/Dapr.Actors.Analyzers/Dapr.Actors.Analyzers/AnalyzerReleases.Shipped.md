76bfed2ce8dd1c29fe37899783090cf882a6876a
Nils Gruson <ngruson@hotmail.com>
2025-04-10 00:45:07 +0200

    Add Roslyn analyzer for actor registration (#1441)
    Added Roslyn analyzers and code fixes for:
    - Encouraging JSON serialization for actors
    - Validating actors are properly registered in DI
    - Ensuring that the actor handler mapping is performed during startup
    
    Signed-off-by: Nils Gruson <ngruson@hotmail.com>
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    Co-authored-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Actors.Analyzers/Dapr.Actors.Analyzers/AnalyzerReleases.Shipped.md
+++ src/Dapr.Actors.Analyzers/Dapr.Actors.Analyzers/AnalyzerReleases.Shipped.md
@@ -7,3 +7,6 @@
 Rule ID | Category | Severity | Notes 
 --------|----------|----------|------------------------------------------------------------------------------------
 DAPR4001 | Usage | Warning | Actor timer method invocations require the named callback method to exist on type.
+DAPR4002 | Usage | Warning | The actor type is not registered with dependency injection
+DAPR4003 | Usage | Info | Set options.UseJsonSerialization to true to support interoperability with non-.NET actors
+DAPR4004 | Usage | Warning | Call app.MapActorsHandlers to map endpoints for Dapr actors.
---
+++ test/Dapr.Actors.Analyzers.Test/ActorJsonSerializationCodeFixProviderTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Dapr.Actors.Analyzers.Test;
+
+public class ActorJsonSerializationCodeFixProviderTests
+{
+    [Fact]
+    public async Task UseJsonSerialization()
+    {
+        var code = @"
+            //using Dapr.Actors.Runtime;
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.Extensions.DependencyInjection;
+            
+            public static class Program
+            {
+                public static void Main()
+                {
+                    var builder = WebApplication.CreateBuilder();
+                        
+                    builder.Services.AddActors(options =>
+                    {                        
+                    });
+
+                    var app = builder.Build();
+
+                    app.MapActorsHandlers();
+                }
+            }
+            ";
+
+        var expectedChangedCode = @"
+            //using Dapr.Actors.Runtime;
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.Extensions.DependencyInjection;
+
+            public static class Program
+            {
+                public static void Main()
+                {
+                    var builder = WebApplication.CreateBuilder();
+
+                    builder.Services.AddActors(options =>
+                    {
+                        options.UseJsonSerialization = true;
+                    });
+
+                    var app = builder.Build();
+
+                    app.MapActorsHandlers();
+                }
+            }
+            ";
+
+        await VerifyCodeFix.RunTest<ActorJsonSerializationCodeFixProvider>(code, expectedChangedCode);
+    }
+}
