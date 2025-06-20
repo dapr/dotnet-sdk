32fa9ed914c8f1da33cfe26c55e7c4700668f0ba
Whit Waldo <whit.waldo@innovian.net>
2025-06-20 03:44:09 -0500

    Jobs overwrite/failure policy updates (#1558)
    Updated Jobs SDK to reflect both the overwrite and failure policy additions

--- src/Dapr.Extensions.Configuration/DaprSecretStoreConfigurationProvider.cs
+++ src/Dapr.Extensions.Configuration/DaprSecretStoreConfigurationProvider.cs
@@ -189,7 +189,7 @@ internal class DaprSecretStoreConfigurationProvider : ConfigurationProvider
 
     private async Task LoadAsync()
     {
-        var data = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        var data = new Dictionary<string, string?>(StringComparer.InvariantCultureIgnoreCase);
 
         // Wait for the Dapr Sidecar to report healthy before attempting to fetch secrets.
         using (var tokenSource = new CancellationTokenSource(sidecarWaitTimeout))
---
+++ src/Dapr.Jobs/Models/JobFailurePolicy.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------------------
+//  Copyright 2025 The Dapr Authors
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ------------------------------------------------------------------------
+
+namespace Dapr.Jobs.Models;
+
+/// <summary>
+/// Specifies the policy the apply when a job fails to trigger.
+/// </summary>
+public enum JobFailurePolicy
+{
+    /// <summary>
+    /// Drops the job tick with the job fails to trigger.
+    /// </summary>
+    Drop,
+    /// <summary>
+    /// Retries the job at a consistent interval when the job fails to trigger.
+    /// </summary>
+    Constant
+}
