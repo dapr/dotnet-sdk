64f4da693586016638a33852a566fd365b96bc5a
Whit Waldo <whit.waldo@innovian.net>
2025-09-01 04:18:10 -0500

    Adding example project for the AI .NET extensions
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ examples/AI/DotNetExtensionsDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dapr.AI.Conversation.Extensions;
+using Dapr.AI.DotnetExtensions;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices(services =>
+{
+    services.AddDaprConversationClient();
+    services.AddDaprChatClient(opt =>
+    {
+        opt.ConversationComponentName = "conversation";
+    });
+});
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var chatClient = scope.ServiceProvider.GetRequiredService<IChatClient>();
+
+var response = await chatClient.GetResponseAsync([
+    new ChatMessage(ChatRole.User,
+        "Please write me a poem in iambic pentameter about the joys of using Dapr to develop distributed applications with .NET")
+]);
+
+foreach (var r in response.Messages)
+{
+    Console.WriteLine(r.Text);
+}
