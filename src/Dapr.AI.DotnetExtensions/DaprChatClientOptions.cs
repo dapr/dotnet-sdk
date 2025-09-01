7cc5355c75a9775d2d061c9b408ddd7b3ff11a8f
Whit Waldo <whit.waldo@innovian.net>
2025-09-01 11:17:27 -0500

    Microsoft.Extensions.AI abstraction for IChatClient in Dapr (#1620)
    * Fixed several issues with the Conversation API implementation to correct missing nullability annotations on proto-generated types
    * Implemented Dapr conversation client atop .NET AI extensions
    * Added components to the AI examples
    * Adding example project for the AI .NET extensions
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.AI.DotnetExtensions/DaprChatClientOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Dapr.AI.DotnetExtensions;
+
+/// <summary>
+/// Provides the required operation for configuring the <see cref="DaprChatClient"/>.
+/// </summary>
+public sealed class DaprChatClientOptions
+{
+    /// <summary>
+    /// The name of the Dapr Conversation component to use. 
+    /// </summary>
+    public required string ConversationComponentName { get; set; }
+}
