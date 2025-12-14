9d7c520238652ba2cb0fcebf78ba79569af969dc
Whit Waldo <whit.waldo@innovian.net>
2025-12-14 11:02:14 -0600

    Extracted more public methods out to interface from implementation
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Workflow/Worker/IWorkflowsFactory.cs
+++ src/Dapr.Workflow/Worker/IWorkflowsFactory.cs
@@ -12,12 +12,47 @@
 //  ------------------------------------------------------------------------
 
 using System;
+using System.Threading.Tasks;
 using Dapr.Workflow.Abstractions;
 
 namespace Dapr.Workflow.Worker;
 
 internal interface IWorkflowsFactory
 {
+    /// <summary>
+    /// Registers a workflow type.
+    /// </summary>
+    /// <typeparam name="TWorkflow">The workflow type to register.</typeparam>
+    /// <param name="name">Optional workflow name. If not specified, uses the type name.</param>
+    void RegisterWorkflow<TWorkflow>(string? name = null) where TWorkflow : class, IWorkflow;
+
+    /// <summary>
+    /// Registers a workflow as a function.
+    /// </summary>
+    /// <typeparam name="TInput">The type of the workflow input.</typeparam>
+    /// <typeparam name="TOutput">The type of the workflow output.</typeparam>
+    /// <param name="name">Workflow name.</param>
+    /// <param name="implementation">Function implementing the workflow definition.</param>
+    void RegisterWorkflow<TInput, TOutput>(string name,
+        Func<WorkflowContext, TInput, Task<TOutput>> implementation);
+
+    /// <summary>
+    /// Registers a workflow activity type.
+    /// </summary>
+    /// <typeparam name="TActivity">The activity type to register.</typeparam>
+    /// <param name="name">Optional activity name. If not specified, uses the type name.</param>
+    void RegisterActivity<TActivity>(string? name = null) where TActivity : class, IWorkflowActivity;
+
+    /// <summary>
+    /// Registers an activity as a function. 
+    /// </summary>
+    /// <param name="name">The name of the activity.</param>
+    /// <param name="implementation">The implementation of the activity.</param>
+    /// <typeparam name="TInput">The type of the input to the activity.</typeparam>
+    /// <typeparam name="TOutput">The type of the output returned from the activity.</typeparam>
+    public void RegisterActivity<TInput, TOutput>(string name,
+        Func<WorkflowActivityContext, TInput, Task<TOutput>> implementation);
+    
     /// <summary>
     /// Tries to create a workflow instance.
     /// </summary>
--- src/Dapr.Workflow/Worker/WorkflowsFactory.cs
+++ src/Dapr.Workflow/Worker/WorkflowsFactory.cs
@@ -30,11 +30,7 @@ internal sealed class WorkflowsFactory(ILogger<WorkflowsFactory> logger) : IWork
     private readonly ConcurrentDictionary<string, Func<IServiceProvider, IWorkflowActivity>> _activityFactories =
         new(StringComparer.OrdinalIgnoreCase);
 
-    /// <summary>
-    /// Registers a workflow type.
-    /// </summary>
-    /// <typeparam name="TWorkflow">The workflow type to register.</typeparam>
-    /// <param name="name">Optional workflow name. If not specified, uses the type name.</param>
+    /// <inheritdoc />
     public void RegisterWorkflow<TWorkflow>(string? name = null) where TWorkflow : class, IWorkflow
     {
         name ??= typeof(TWorkflow).Name;
@@ -48,14 +44,8 @@ internal sealed class WorkflowsFactory(ILogger<WorkflowsFactory> logger) : IWork
             logger.LogRegisterWorkflowAlreadyRegistered(name);
         }
     }
-
-    /// <summary>
-    /// Registers a workflow as a function.
-    /// </summary>
-    /// <typeparam name="TInput">The type of the workflow input.</typeparam>
-    /// <typeparam name="TOutput">The type of the workflow output.</typeparam>
-    /// <param name="name">Workflow name.</param>
-    /// <param name="implementation">Function implementing the workflow definition.</param>
+    
+    /// <inheritdoc />
     public void RegisterWorkflow<TInput, TOutput>(string name,
         Func<WorkflowContext, TInput, Task<TOutput>> implementation)
     {
@@ -71,12 +61,8 @@ internal sealed class WorkflowsFactory(ILogger<WorkflowsFactory> logger) : IWork
             logger.LogRegisterWorkflowAlreadyRegistered(name);
         }
     }
-
-    /// <summary>
-    /// Registers a workflow activity type.
-    /// </summary>
-    /// <typeparam name="TActivity">The activity type to register.</typeparam>
-    /// <param name="name">Optional activity name. If not specified, uses the type name.</param>
+    
+    /// <inheritdoc />
     public void RegisterActivity<TActivity>(string? name = null) where TActivity : class, IWorkflowActivity
     {
         name ??= typeof(TActivity).Name;
@@ -91,13 +77,7 @@ internal sealed class WorkflowsFactory(ILogger<WorkflowsFactory> logger) : IWork
         }
     }
 
-    /// <summary>
-    /// Registers an activity as a function. 
-    /// </summary>
-    /// <param name="name">The name of the activity.</param>
-    /// <param name="implementation">The implementation of the activity.</param>
-    /// <typeparam name="TInput">The type of the input to the activity.</typeparam>
-    /// <typeparam name="TOutput">The type of the output returned from the activity.</typeparam>
+    /// <inheritdoc />
     public void RegisterActivity<TInput, TOutput>(string name,
         Func<WorkflowActivityContext, TInput, Task<TOutput>> implementation)
     {
