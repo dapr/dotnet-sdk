9f16a6ec9ddaa62d04c0b14bebe37dcaf17e51d4
Whit Waldo <whit.waldo@innovian.net>
2025-12-14 12:56:17 -0600

    Removed unnecessary imports
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- test/Dapr.Workflow.Analyzers.Test/Utilities.cs
+++ test/Dapr.Workflow.Analyzers.Test/Utilities.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
-using System.Reflection;
 using Dapr.Analyzers.Common;
 using Microsoft.Extensions.Hosting;
 
@@ -25,10 +22,8 @@ internal static class Utilities
         metadataReferences.AddRange(TestUtilities.GetAllReferencesNeededForType(typeof(Workflow<,>)));
         metadataReferences.AddRange(TestUtilities.GetAllReferencesNeededForType(typeof(WorkflowActivity<,>)));
         metadataReferences.Add(MetadataReference.CreateFromFile(typeof(Task).Assembly.Location));
-        metadataReferences.Add(MetadataReference.CreateFromFile(typeof(WebApplication).Assembly.Location));
         metadataReferences.Add(MetadataReference.CreateFromFile(typeof(DaprWorkflowClient).Assembly.Location));
         metadataReferences.Add(MetadataReference.CreateFromFile(typeof(Microsoft.Extensions.DependencyInjection.ServiceCollection).Assembly.Location));
-        metadataReferences.Add(MetadataReference.CreateFromFile(typeof(IApplicationBuilder).Assembly.Location));
         metadataReferences.Add(MetadataReference.CreateFromFile(typeof(IHost).Assembly.Location));
         return metadataReferences;
     }
