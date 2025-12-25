6a217402a1b9b078ca01a0b92209bf335bc67c78
Whit Waldo <whit.waldo@innovian.net>
2025-12-25 14:57:11 -0600

    Modified CICD to create scheduler path with appropriate permissions - inside that, each test will create a subdirectory that can be created and deleted per test.
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.TestContainers/Common/TestDirectoryManager.cs
@@ -0,0 +1,52 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2025 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//  ------------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+namespace Dapr.TestContainers.Common;
+
+internal static class TestDirectoryManager
+{
+    private const string GlobalRoot = "/tmp/dapr-scheduler";
+
+    public static string CreateTemporaryTestDirectory()
+    {
+        var uniqueTestDir = Path.Combine(GlobalRoot, Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(uniqueTestDir);
+        
+        // For Linux : Ensure directory has appropriate permissions
+        if (Environment.OSVersion.Platform == PlatformID.Unix)
+        {
+            var processStartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "chmod",
+                Arguments = "-R 777 " + uniqueTestDir,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            System.Diagnostics.Process.Start(processStartInfo)?.WaitForExit();
+        }
+
+        return uniqueTestDir;
+    }
+    
+    public static void CleanUpTemporaryTestDirectory(string directoryPath)
+    {
+        if (Directory.Exists(directoryPath))
+        {
+            Directory.Delete(directoryPath, true);
+        }
+    }
+}
--- src/Dapr.TestContainers/Containers/Dapr/DaprSchedulerContainer.cs
+++ src/Dapr.TestContainers/Containers/Dapr/DaprSchedulerContainer.cs
@@ -32,7 +32,8 @@ public sealed class DaprSchedulerContainer : IAsyncStartable
 {
 	private readonly IContainer _container;
     // Contains the data directory used by this instance of the Dapr scheduler service
-    private readonly string _hostDataDir = Path.Combine(Path.GetTempPath(), $"dapr-scheduler-{Guid.NewGuid():N}");
+    //private readonly string _hostDataDir = Path.Combine(Path.GetTempPath(), $"dapr-scheduler-{Guid.NewGuid():N}");
+    private readonly string _testDirectory;
     private readonly string _containerName = $"scheduler-{Guid.NewGuid():N}";
 
     /// <summary>
@@ -66,8 +67,7 @@ public sealed class DaprSchedulerContainer : IAsyncStartable
             "--etcd-data-dir", containerDataDir
         ];
 
-        //Create a unique temp directory on the host for the test run based on this instance's data directory
-        var directory = EnsureDataDirectory();
+        _testDirectory = TestDirectoryManager.CreateTemporaryTestDirectory();
         
         _container = new ContainerBuilder()
 			.WithImage(options.SchedulerImageTag)
@@ -76,36 +76,10 @@ public sealed class DaprSchedulerContainer : IAsyncStartable
             .WithCommand(cmd.ToArray())
 			.WithPortBinding(InternalPort, assignRandomHostPort: true)
             // Mount an anonymous volume to /data to ensure the scheduler has write permissions
-            .WithBindMount(directory, containerDataDir, AccessMode.ReadWrite)
+            .WithBindMount(_testDirectory, containerDataDir, AccessMode.ReadWrite)
             .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("api is ready"))
 			.Build();
 	}
-
-    private static string EnsureDataDirectory()
-    {
-        var dataDir = Path.Combine(Path.GetTempPath(), "dapr-scheduler", "default-dapr-scheduler-server-0");
-        if (!Directory.Exists(dataDir))
-        {
-            // Ensuring the directory exists with appropriate permissions
-            Directory.CreateDirectory(dataDir);
-
-            // Set permissions if on Linux
-            if (Environment.OSVersion.Platform == PlatformID.Unix)
-            {
-                var processStartInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    FileName = "chmod",
-                    Arguments = "-R 777 " + dataDir,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                };
-                System.Diagnostics.Process.Start(processStartInfo)?.WaitForExit();
-            }
-        }
-
-        return dataDir;
-    }
     
     /// <inheritdoc />
 	public async Task StartAsync(CancellationToken cancellationToken = default)
@@ -120,8 +94,10 @@ public sealed class DaprSchedulerContainer : IAsyncStartable
 	public ValueTask DisposeAsync()
     {
         // Remove the data directory if it exists
-        if (Directory.Exists(_hostDataDir))
-            Directory.Delete(_hostDataDir, true);
+        TestDirectoryManager.CleanUpTemporaryTestDirectory(_testDirectory);
+        
+        // if (Directory.Exists(_hostDataDir))
+        //     Directory.Delete(_hostDataDir, true);
         return _container.DisposeAsync();
     }
 }
