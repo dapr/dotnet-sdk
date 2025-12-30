b4c226297e12c1bd6cae47fca0b96a289b28002f
Whit Waldo <whit.waldo@innovian.net>
2025-12-30 12:25:37 -0600

    Adding job management integration test
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ test/Dapr.IntegrationTest.Jobs/JobManagementTests.cs
@@ -0,0 +1,66 @@
+﻿// ------------------------------------------------------------------------
+// Copyright 2025 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//  ------------------------------------------------------------------------
+
+using Dapr.Jobs;
+using Dapr.Jobs.Extensions;
+using Dapr.Jobs.Models;
+using Dapr.TestContainers.Common;
+using Dapr.TestContainers.Common.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dapr.E2E.Test.Jobs;
+
+public sealed class JobManagementTests
+{
+    [Fact]
+    public async Task ShouldScheduleJobWithTTL()
+    {
+        var options = new DaprRuntimeOptions();
+        var componentsDir = TestDirectoryManager.CreateTestDirectory("jobs-component");
+        var jobName = $"ttl-job-{Guid.NewGuid():N}";
+
+        var harness = new DaprHarnessBuilder(options).BuildJobs(componentsDir);
+        await using var testApp = await DaprHarnessBuilder.ForHarness(harness)
+            .ConfigureServices(builder =>
+            {
+                builder.Services.AddDaprJobsClient(configure: (sp, clientBuilder) =>
+                {
+                    var config = sp.GetRequiredService<IConfiguration>();
+                    var grpcEndpoint = config["DAPR_GRPC_ENDPOINT"];
+                    var httpEndpoint = config["DAPR_HTTP_ENDPOINT"];
+
+                    if (!string.IsNullOrEmpty(grpcEndpoint))
+                        clientBuilder.UseGrpcEndpoint(grpcEndpoint);
+                    if (!string.IsNullOrEmpty(httpEndpoint))
+                        clientBuilder.UseHttpEndpoint(httpEndpoint);
+                });
+            })
+            .BuildAndStartAsync();
+
+        using var scope = testApp.CreateScope();
+        var daprJobsClient = scope.ServiceProvider.GetRequiredService<DaprJobsClient>();
+
+        var startTime = DateTimeOffset.UtcNow.AddSeconds(2);
+        var ttl = DateTimeOffset.UtcNow.AddMinutes(5);
+
+        await daprJobsClient.ScheduleJobAsync(jobName, DaprJobSchedule.FromDuration(TimeSpan.FromSeconds(1)),
+            startingFrom: startTime, ttl: ttl, repeats: 100, overwrite: true);
+
+        var jobDetails = await daprJobsClient.GetJobAsync(jobName);
+        Assert.NotNull(jobDetails);
+        Assert.NotNull(jobDetails.Ttl);
+        
+        await daprJobsClient.DeleteJobAsync(jobName);
+    }
+}
