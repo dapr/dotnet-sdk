6490420d71d8b26063b786ae3f5a58be25ce87ea
Whit Waldo <whit.waldo@innovian.net>
2026-01-17 16:27:49 -0600

    Re-added Dapr.Testcontainers.Test project
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ test/Dapr.Testcontainers.Test/Harnesses/DaprTestEnvironmentTests.cs
@@ -0,0 +1,21 @@
+﻿using Dapr.Testcontainers.Harnesses;
+using Xunit;
+
+namespace Dapr.Testcontainers.Test.Harnesses;
+
+public sealed class DaprTestEnvironmentTests
+{
+    [Fact]
+    public void Constructor_ShouldExposeRedisContainer_WhenNeedsActorStateIsTrue()
+    {
+        var env = new DaprTestEnvironment(needsActorState: true);
+        Assert.NotNull(env.RedisContainer);
+    }
+
+    [Fact]
+    public void Constructor_ShouldNotExposeRedisContainer_WhenNeedsActorStateIsFalse()
+    {
+        var env = new DaprTestEnvironment(needsActorState: false);
+        Assert.Null(env.RedisContainer);
+    }
+}
