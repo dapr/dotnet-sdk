65c46a895e50e987de858e6a338f79ea4de591fe
Whit Waldo <whit.waldo@innovian.net>
2026-02-05 00:54:44 -0600

    Added script for testing and building tag-selector for retrieving Dapr runtime tags so they can be automatically retrieved at CI time
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ .github/tools/tag-selector/src/index.ts
@@ -0,0 +1,39 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { computeFromTags } from "./lib";
+
+async function run() {
+    try {
+        const token = core.getInput("github_token", { required: true});
+        const tagPrefix = core.getIndex("tag_prefix") || "";
+        const stableCount = parseInt(core.getInput("stable_count") || "2", 10);
+        const rcIdent = core.getInput("rc_identifier") || "rc";
+        
+        const ctx = github.context;
+        const owner = core.getInput("owner") || cx.repo.owner;
+        const repo = core.getInput("repo") || ctx.repo.repo;
+        
+        const octokit = github.getOctokit(token);
+        
+        // Paginate all tags
+        const tags: Tag[] = await octokit.paginate(octokit.rest.repos.listTags, {
+            owner,
+            repo,
+            per_page: 100
+        });
+        
+        const tagNames = tags.map((t: { name: string }) => t.name);
+        const result = computeFromTags({
+            tags: tagNames,
+            tagPrefix,
+            stableCount,
+            rcIdent,
+        });
+
+        core.setOutput("matrix_json", JSON.stringify(result.matrix_json));
+    } catch (err: any) {
+        core.setFailed(err?.message ?? String(err));
+    }
+}
+
+run();
