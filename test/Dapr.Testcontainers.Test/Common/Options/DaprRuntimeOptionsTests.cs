ea2cdf3c17de96c357f4f68bf987f8accd0bca63
Whit Waldo <whit.waldo@innovian.net>
2026-02-05 04:08:06 -0600

    Infra: Modifying CI to validate integration tests against current stable and RC Dapr runtime versions (#1693)
    * Modifying to optionally use an envvar to get the Dapr version if set
    * Added unit tests to validate expected values
    * Added script for testing and building tag-selector for retrieving Dapr runtime tags so they can be automatically retrieved at CI time
    * Updated to add step for building and running the script to get Dapr runtime tags
    * Updated paths
    * Updated tsconfig, fixed a bug in inputs and rebuilt output
    * Simplified GitHub action name for integration tests v2 to reflect {tfm}-{Dapr runtime}-{ProjectName}
    * Drops "Dapr.IntegrationTest." from start of the project name
    * Trying to correct transient port unavailability issue
    * Update to include the most recent two stable versions so as to prevent non-RC regressions for minor releases.
    
    ---------
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ test/Dapr.Testcontainers.Test/Common/Options/DaprRuntimeOptionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Dapr.Testcontainers.Common.Options;
+
+namespace Dapr.Testcontainers.Test.Common.Options;
+
+public sealed class DaprRuntimeOptionsTests : IDisposable
+{
+    private const string DaprRuntimeVersionEnvVarName = "DAPR_RUNTIME_VERSION";
+    
+    [Fact]
+    public void ShouldUseEnvVarVersion_IfSet()
+    {
+        const string version = "1.16.2";
+        Environment.SetEnvironmentVariable(DaprRuntimeVersionEnvVarName, version);
+
+        var options = new DaprRuntimeOptions();
+        Assert.Equal(version, options.Version);
+    }
+
+    [Fact]
+    public void ShouldUseDefaultVersionIfEnvVarNotTest()
+    {
+        const string defaultValue = "latest";
+
+        var options = new DaprRuntimeOptions();
+        Assert.Equal(defaultValue, options.Version);
+    }
+
+    public void Dispose()
+    {
+        // Clear this variable
+        Environment.SetEnvironmentVariable(DaprRuntimeVersionEnvVarName, null);
+    }
+}
