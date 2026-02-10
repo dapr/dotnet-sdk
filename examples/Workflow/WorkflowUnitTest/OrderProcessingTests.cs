033efa1e2e73d90f41acc26b83205f0beda769d3
Daan Schutte <daan.schutte@icloud.com>
2026-02-10 16:28:42 +0300

    Add TestActivityException
    Signed-off-by: Daan Schutte <daan.schutte@icloud.com>

--- examples/Workflow/WorkflowUnitTest/OrderProcessingTests.cs
+++ examples/Workflow/WorkflowUnitTest/OrderProcessingTests.cs
@@ -88,4 +88,53 @@ public class OrderProcessingTests
             ctx => ctx.CallActivityAsync(nameof(NotifyActivity), It.IsAny<Notification>(), It.IsAny<WorkflowTaskOptions>()),
             Times.Exactly(2));
     }
+
+    [Fact]
+    public async Task TestActivityException()
+    {
+        OrderPayload order = new(Name: "Paperclips", TotalCost: 99.95, Quantity: 10);
+        PaymentRequest expectedPaymentRequest = new(It.IsAny<string>(), order.Name, order.Quantity, order.TotalCost);
+        InventoryRequest expectedInventoryRequest = new(It.IsAny<string>(), order.Name, order.Quantity);
+        InventoryResult inventoryResult = new(Success: true, null);
+
+        Mock<WorkflowContext> mockContext = new();
+        // Mock the call to ReserveInventoryActivity
+        mockContext
+            .Setup(ctx => ctx.CallActivityAsync<InventoryResult>(nameof(ReserveInventoryActivity), It.IsAny<InventoryRequest>(), It.IsAny<WorkflowTaskOptions>()))
+            .Returns(Task.FromResult(inventoryResult));
+        // Throws a WorkflowTaskFailedException on UpdateInventoryActivity
+        mockContext
+            .Setup(ctx => ctx.CallActivityAsync(nameof(UpdateInventoryActivity), It.IsAny<PaymentRequest>(),It.IsAny<WorkflowTaskOptions>()))
+            .Returns(Task.FromException(new WorkflowTaskFailedException("fail", new WorkflowTaskFailureDetails("type", "message"))));
+        mockContext
+            .Setup(ctx => ctx.CreateReplaySafeLogger<OrderProcessingWorkflow>())
+            .Returns(NullLogger<OrderProcessingWorkflow>.Instance);
+        
+        // Run the workflow directly
+        OrderResult result = await new OrderProcessingWorkflow().RunAsync(mockContext.Object, order);
+
+        // Verify that workflow result matches what we expect (not processed)
+        Assert.NotNull(result);
+        Assert.False(result.Processed);
+        
+        // Verify that ReserveInventoryActivity was called with a specific input
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync<InventoryResult>(nameof(ReserveInventoryActivity), expectedInventoryRequest, It.IsAny<WorkflowTaskOptions>()),
+            Times.Once());
+        
+        // Verify that ProcessPaymentActivity was called with a specific input
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(ProcessPaymentActivity), expectedPaymentRequest, It.IsAny<WorkflowTaskOptions>()),
+            Times.Once());
+        
+        // Verify that UpdateInventoryActivity was called with a specific input
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(UpdateInventoryActivity), expectedPaymentRequest, It.IsAny<WorkflowTaskOptions>()),
+            Times.Once());
+        
+        // Verify that there were two calls to NotifyActivity
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(NotifyActivity), It.IsAny<Notification>(), It.IsAny<WorkflowTaskOptions>()),
+            Times.Exactly(2));
+    }
 }
