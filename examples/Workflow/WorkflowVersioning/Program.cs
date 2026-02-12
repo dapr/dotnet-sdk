1ecb7c50dfc2f84d94683f6b5c93983e52b512db
Whit Waldo <whit.waldo@innovian.net>
2026-02-12 11:43:13 -0600

    Added more tests
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- examples/Workflow/WorkflowVersioning/Program.cs
+++ examples/Workflow/WorkflowVersioning/Program.cs
@@ -1,8 +1,6 @@
 using Dapr.Workflow;
 using Dapr.Workflow.Versioning;
 using WorkflowVersioning.Services;
-using WorkflowVersioning.Versioning;
-using WorkflowVersioning.Workflows.VacationApproval;
 using WorkflowVersioning.Workflows.VacationApproval.Activities;
 using WorkflowVersioning.Workflows.VacationApproval.Models;
 
@@ -10,11 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IEmailService, EmailService>();
 
-builder.Services.AddDaprWorkflowVersioning(v =>
+builder.Services.AddDaprWorkflowVersioning();
+
+// By default, it registers a numerical versioning strategy - let's demonstrate overriding this with a date-based approach
+// and non-standard options
+const string optionsName = "workflow-defaults";
+builder.Services.UseDefaultWorkflowStrategy<NumericVersionStrategy>(optionsName);
+builder.Services.ConfigureStrategyOptions<NumericVersionStrategyOptions>(optionsName, o =>
 {
-    v.DefaultStrategy = _ => new NumericalStrategy();
-    v.DefaultSelector = _ => new MaxVersionSelector();
+    o.SuffixPrefix = "V";
 });
+
 builder.Services.AddDaprWorkflow(w =>
 {
     w.RegisterActivity<SendEmailActivity>();
@@ -25,7 +29,10 @@ var app = builder.Build();
 app.MapGet("/start/{workflowId}",
     async (DaprWorkflowClient workflowClient, [AsParameters] VacationRequest request, string workflowId) =>
     {
-        await workflowClient.ScheduleNewWorkflowAsync(nameof(VacationApprovalWorkflow), workflowId, request);
+        await workflowClient.ScheduleNewWorkflowAsync("VacationApprovalWorkflow", workflowId, request);
+        var a = 0;
+        a++;
+
     });
 
 app.MapGet("/approve/{workflowId}", async (DaprWorkflowClient workflowClient, string workflowId) =>
