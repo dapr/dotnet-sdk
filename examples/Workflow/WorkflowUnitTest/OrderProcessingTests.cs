88bc43233a873b20c86c60b72a6c4d593fe937f0
Daan Schutte <daan.schutte@icloud.com>
2026-02-13 07:40:27 +0300

    Add TestHighCostOrderApprovalTimeout
    Signed-off-by: Daan Schutte <daan.schutte@icloud.com>

--- examples/Workflow/WorkflowUnitTest/OrderProcessingTests.cs
+++ examples/Workflow/WorkflowUnitTest/OrderProcessingTests.cs
@@ -57,8 +57,9 @@ public class OrderProcessingTests
     [Fact]
     public async Task TestHighCostOrderApproved()
     {
+        // Test payloads
         OrderPayload order = new OrderPayload(Name: "Cars", TotalCost: 100000, Quantity: 2);
-        InventoryResult inventoryResult = new(Success: true, new InventoryItem(order.Name, 50000, order.Quantity));
+        InventoryResult inventoryResult = new(Success: true, null);
         PaymentRequest expectedPaymentRequest = new(It.IsAny<string>(), order.Name, order.Quantity, order.TotalCost);
         InventoryRequest expectedInventoryRequest = new InventoryRequest(It.IsAny<string>(), order.Name, order.Quantity);
         
@@ -114,6 +115,55 @@ public class OrderProcessingTests
     }
 
     [Fact]
+    public async Task TestHighCostOrderApprovalTimeout()
+    {
+        // Test payloads
+        OrderPayload order = new OrderPayload(Name: "Cars", TotalCost: 100000, Quantity: 2);
+        InventoryResult inventoryResult = new(Success: true, null);
+        InventoryRequest expectedInventoryRequest = new(It.IsAny<string>(), order.Name, order.Quantity);
+
+        Mock<WorkflowContext> mockContext = new();
+        // Mock the call to ReserveInventoryActivity
+        mockContext
+            .Setup(ctx => ctx.CallActivityAsync<InventoryResult>(nameof(ReserveInventoryActivity), It.IsAny<InventoryRequest>(), It.IsAny<WorkflowTaskOptions>()))
+            .Returns(Task.FromResult(inventoryResult));
+        // Mock a timeout after waiting for approval
+        mockContext
+            .Setup(ctx => ctx.WaitForExternalEventAsync<ApprovalResult>(It.IsAny<string>(), It.IsAny<TimeSpan>()))
+            .Returns(Task.FromException<ApprovalResult>(new TaskCanceledException()));
+        mockContext
+            .Setup(ctx => ctx.CreateReplaySafeLogger<OrderProcessingWorkflow>())
+            .Returns(NullLogger<OrderProcessingWorkflow>.Instance);
+        
+        // Run the workflow directly
+        OrderResult result = await new OrderProcessingWorkflow().RunAsync(mockContext.Object, order);
+
+        // Verify that workflow result matches what we expect (not processed)
+        Assert.NotNull(result);
+        Assert.False(result.Processed);
+        
+        // Verify that ReserveInventoryActivity was called with a specific input
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync<InventoryResult>(nameof(ReserveInventoryActivity), expectedInventoryRequest, It.IsAny<WorkflowTaskOptions>()),
+            Times.Once());
+        
+        // Verify that ProcessPaymentActivity was not called
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(ProcessPaymentActivity), It.IsAny<PaymentRequest>(), It.IsAny<WorkflowTaskOptions>()),
+            Times.Never());
+        
+        // Verify that UpdateInventoryActivity was not called
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(UpdateInventoryActivity), It.IsAny<PaymentRequest>(), It.IsAny<WorkflowTaskOptions>()),
+            Times.Never());
+        
+        // Verify that there were two calls to NotifyActivity
+        mockContext.Verify(
+            ctx => ctx.CallActivityAsync(nameof(NotifyActivity), It.IsAny<Notification>(), It.IsAny<WorkflowTaskOptions>()),
+            Times.Exactly(2));
+    }
+
+    [Fact]
     public async Task TestInsufficientInventory()
     {
         // Test payloads
@@ -152,6 +202,7 @@ public class OrderProcessingTests
     [Fact]
     public async Task TestActivityException()
     {
+        // Test payloads
         OrderPayload order = new(Name: "Paperclips", TotalCost: 99.95, Quantity: 10);
         PaymentRequest expectedPaymentRequest = new(It.IsAny<string>(), order.Name, order.Quantity, order.TotalCost);
         InventoryRequest expectedInventoryRequest = new(It.IsAny<string>(), order.Name, order.Quantity);
