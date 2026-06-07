3bcc40e4545c77650e2cc579a9722e26e174ebd4
Whit Waldo <whit.waldo@innovian.net>
2026-06-07 17:25:29 -0500

    Added metadata runtime implementation
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ src/Dapr.Metadata.Runtime/DaprMetadataChangeTokenSource.cs
@@ -0,0 +1,12 @@
+﻿using Dapr.Metadata.Abstractions;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+
+namespace Dapr.Metadata.Runtime;
+
+internal sealed class DaprMetadataChangeTokenSource(DaprMetadataRefreshSignal signal) : IOptionsChangeTokenSource<DaprMetadata>
+{
+    public IChangeToken GetChangeToken() => signal.GetChangeToken();
+
+    public string? Name => Options.DefaultName;
+}
---
+++ src/Dapr.Metadata.Runtime/IDaprMetadataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Dapr.Metadata.Abstractions;
+
+namespace Dapr.Metadata.Runtime;
+
+/// <summary>
+/// Represents a provider for retrieving metadata from the Dapr runtime.
+/// </summary>
+internal interface IDaprMetadataProvider
+{
+    /// <summary>
+    /// Retrieves the metadata value from the Dapr runtime.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>An instance of a <see cref="DaprMetadata"/>.</returns>
+    ValueTask<DaprMetadata> GetAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Used to force an on-demand data refresh.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>An instance of a <see cref="DaprMetadata"/>.</returns>
+    ValueTask<DaprMetadata> RefreshAsync(CancellationToken cancellationToken = default);
+}
