7956d457d01a8e2546d1e07900125a17beac7797
Whit Waldo <whit.waldo@innovian.net>
2026-06-10 12:53:28 -0500

    Ensuring that GetWorkflowStateAsync always returns a `WorkflowState` so the `WorkflowState.Exists` properly reflects the presence of the value (or not).
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- src/Dapr.Workflow/DaprWorkflowClient.cs
+++ src/Dapr.Workflow/DaprWorkflowClient.cs
@@ -138,12 +138,12 @@ public sealed class DaprWorkflowClient : IDaprWorkflowClient
         try
         {
             var metadata = await _innerClient.GetWorkflowMetadataAsync(instanceId, getInputsAndOutputs, cancellation);
-            return metadata is null ? null : new WorkflowState(metadata);    
+            return new WorkflowState(metadata);    
         }
         catch (RpcException)
         {
             // If the state doesn't exist, we typically get an `RpcException`, so this wraps this and just returns null
-            return null;
+            return new WorkflowState(null);
         }
     }
     
